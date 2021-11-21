18f360bdb0032f3f205944cfb3c2adb96f2f6790
Ellie <62729616+elliesec@users.noreply.github.com>
2021-11-21 09:34:33 +0000

    [CHANGE] Log directory and application startup organisation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import { mkdirSync } from 'fs';
 import { APP_NAME } from './config';
 import { AddFileOutput, GetLogger, LogLevel, SetConsoleOutput } from './logging';
 
-SetConsoleOutput(LogLevel.DEBUG);
-AddFileOutput('./debug.log', false, LogLevel.DEBUG);
-AddFileOutput('./error.log', true, LogLevel.ALERT);
-
+const LOG_DIR = './logs';
 const logger = GetLogger('init');
 
-logger.info(`${ APP_NAME } starting...`);
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+start();
+
+/**
+ * Starts the application.
+ */
+function start(): void {
+	setupLogging();
+	logger.info(`${APP_NAME} starting...`);
+}
+
+/**
+ * Configures logging for the application.
+ */
+function setupLogging(): void {
+	mkdirSync(LOG_DIR, { recursive: true });
+	SetConsoleOutput(LogLevel.DEBUG);
+	AddFileOutput(`${LOG_DIR}/debug.log`, false, LogLevel.DEBUG);
+	AddFileOutput(`${LOG_DIR}/error.log`, true, LogLevel.ALERT);
+}

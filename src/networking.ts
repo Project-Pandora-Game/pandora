ec3f9c5d643424f4f2fc61e4bbc22bab6d824ec9
Jomshir98 <jomshir98@protonmail.com>
2022-01-17 20:44:19 +0100

    [ADD] HTTP server and Shard to Directory connection

---
+++ src/networking.ts
@@ -0,0 +1,4 @@
+/** HTTP path the shard connects to when connecting to Direcotory */
+export const HTTP_SOCKET_IO_SHARD_PATH: string = '/pandora_shard_socket.io';
+/** Name of HTTP header containing Shard's shared secret to authenticate to Directory */
+export const HTTP_HEADER_SHARD_SECRET: string = 'Pandora-Shard-Secret';

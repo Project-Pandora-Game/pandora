7d1eaec48db0aa8c12874cecd55fe6c20a821aaa
TechTheAwesome <technicalcreepers@gmail.com>
2022-01-24 00:16:33 +0900

    [ADD] Jest, CI workflow, and VScode on-save formatting
    * [Change] .vscode/settings.json to use eslint on-save
    
    * [ADD] Jest for unit testing
    
    * [FIX] maxWorker & tabs issue
    
    * [FIX] split tsconfig into dev & build
    
    * [Change] package.json based on suggestion
    
    Co-authored-by: Jomshir98 <jomshir98@protonmail.com>
    
    * [ADD] Github CI workflow for test
    
    Co-authored-by: Jomshir98 <jomshir98@protonmail.com>

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function TrueFunc(): boolean {
+	return true;
+}
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { TrueFunc } from '../src/index';
+
+describe('Sanity', () => {
+	it('should be checked', () => {
+		expect(TrueFunc()).toBeTruthy();
+	});
+});

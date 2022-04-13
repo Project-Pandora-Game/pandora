0e363306d5f02c2ce6a9899be268acc1203d5731
Sekkmer <sekkmer@gmail.com>
2022-04-13 19:49:39 +0200

    [ADD] post intall script and git hooks
    * [ADD] postinstall to copy env template
    
    only copies if the .env file does not exists and we have an .env.template file
    
    * [ADD] git hook install script
    
    * [ADD] git hook to prevent commit against master brach

---
+++ scripts/postinstall.js
@@ -0,0 +1,42 @@
+/* Scripts are run in Node, so don't make use of the logger or ES imports */
+/* eslint-disable no-console, @typescript-eslint/no-var-requires*/
+const { constants } = require('fs');
+const { copyFile } = require('fs/promises');
+const { resolve } = require('path');
+const { spawnSync } = require('child_process');
+
+postinstall();
+
+async function postinstall() {
+	await copyDotenv();
+	configureGitHooks();
+}
+
+async function copyDotenv() {
+	try {
+		await copyFile(
+			resolve(__dirname, '..', '.env.template'),
+			resolve(__dirname, '..', '.env'),
+			constants.COPYFILE_EXCL,
+		);
+		console.log('No .env file found - template copied');
+	} catch (error) {
+		if (error.code !== 'EEXIST' && error.code !== 'ENOENT') {
+			throw error;
+		}
+	}
+}
+
+function configureGitHooks() {
+	const requiredPath = resolve(__dirname, '..', '.hooks');
+	const { stdout } = spawnSync('git', ['config', 'core.hooksPath']);
+	const hooksPath = stdout.toString().trim();
+	if (hooksPath === requiredPath)
+		return;
+
+	const { error } = spawnSync('git', ['config', 'core.hooksPath', requiredPath]);
+	if (error)
+		throw error;
+
+	console.log('Git hooks path configured');
+}

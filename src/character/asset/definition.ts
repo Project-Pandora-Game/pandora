bc42b65ebcca2770209daa78ef4f220f86116678
Sekkmer <sekkmer@gmail.com>
2022-05-02 21:00:25 +0200

    [ADD] layer mirroring
    * [ADD] position size and mirroring for layers
    
    * [ADD] point type with selective mirror

--- src/character/asset/definition.ts
+++ src/character/asset/definition.ts
@@ -3,6 +3,12 @@ import type { ArrayCompressType } from '../../utility';
 export type Coordinates = { x: number, y: number; };
 export type CoordinatesCompressed = ArrayCompressType<Coordinates, ['x', 'y']>;
 
+export type Size = { width: number, height: number; };
+export type SizeCompressed = ArrayCompressType<Size, ['width', 'height']>;
+
+export type Rectangle = Coordinates & Size;
+export type RectangleCompressed = [...CoordinatesCompressed, ...SizeCompressed];
+
 export type ConditionOperator = '=' | '<' | '<=' | '>' | '>=' | '!=';
 export interface AtomicCondition {
 	bone: string;
@@ -45,11 +51,13 @@ export interface PointDefinition {
 	pos: CoordinatesCompressed;
 	transforms: TransformDefinition[];
 	mirror: boolean;
+	pointType?: string;
 }
 export type PointDefinitionCompressed = {
 	pos: CoordinatesCompressed;
 	transforms?: TransformDefinitionCompressed[];
 	mirror?: true;
+	pointType?: string;
 };
 export type LayerImageOverride = { image: string; condition: Condition; };
 export type LayerImageOverrideCompressed = ArrayCompressType<LayerImageOverride, ['image', 'condition'], { condition: ConditionCompressed; }>;
@@ -63,17 +71,34 @@ export const enum LayerPriority {
 	ABOVE_ARMS,
 	OVERLAY,
 }
-export type LayerDefinition = {
+export const enum LayerMirror {
+	NONE,
+	/** Only imageOverrides are mirrored, points are selected */
+	SELECT,
+	/** Mirrors everything and creates the mirrored image */
+	FULL,
+}
+export const enum LayerSide {
+	LEFT,
+	RIGHT,
+}
+export type LayerDefinition = Rectangle & {
 	image: string;
 	priority: LayerPriority;
 	points: PointDefinition[];
 	imageOverrides: LayerImageOverride[];
+	mirror: LayerMirror;
+	pointType?: string[];
+	side?: LayerSide;
 };
 export type LayerDefinitionCompressed = {
+	rect: RectangleCompressed;
 	image: string;
 	priority: LayerPriority;
-	points: PointDefinitionCompressed[];
+	points: PointDefinitionCompressed[] | string;
 	imageOverrides?: LayerImageOverrideCompressed[];
+	pointType?: string[];
+	mirror: LayerMirror;
 };
 export type AssetSourceType = 'asset';
 export type AssetId = `${AssetSourceType}-${string}`;

0bf3901cde055fc74f9f9e01870b8018b04d714a
Jomshir98 <jomshir98@protonmail.com>
2022-06-14 22:13:09 +0200

    [ADD] Event loop utilization metric

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,4 +1,5 @@
 import promClient from 'prom-client';
+import { performance } from 'perf_hooks';
 import express, { Request, Response } from 'express';
 
 /** Host the metrics as express endpoint */
@@ -17,3 +18,20 @@ promClient.register.setDefaultLabels({
 	app: 'pandora_directory',
 });
 promClient.collectDefaultMetrics({ prefix: 'pandora_directory_' });
+
+// Collect event loop timings
+// Not part of default metrics yet: https://github.com/siimon/prom-client/issues/399, https://github.com/siimon/prom-client/pull/474
+if (performance.nodeTiming && performance.nodeTiming.idleTime !== undefined) {
+	let lastIdle = performance.nodeTiming.idleTime;
+
+	new promClient.Counter({
+		name: 'pandora_directory_' + 'nodejs_eventloop_idle_seconds_total',
+		help: "Total amount of time the event loop has been idle within the event loop's event provider",
+		aggregator: 'average',
+		collect() {
+			const val = performance.nodeTiming.idleTime / 1e3;
+			this.inc(val - lastIdle);
+			lastIdle = val;
+		},
+	});
+}

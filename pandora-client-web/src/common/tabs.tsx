21e7647ef5829edffec424e7717e144b1459e74a
jomshir98 <jomshir98@protonmail.com>
2022-09-01 19:59:29 +0000

    [ADD] More tab improvements, nullable tabs

--- pandora-client-web/src/common/tabs.tsx
+++ pandora-client-web/src/common/tabs.tsx
@@ -9,19 +9,19 @@ interface TabProps extends ChildrenProps {
 }
 
 export function TabContainer({ children, id, className }: {
-	children: ReactElement<TabProps>[]
+	children: (ReactElement<TabProps> | undefined | null)[]
 	id?: string;
 	className?: string;
 }): ReactElement {
 
 	const [currentTab, setTab] = useState(0);
 
-	const tabs = useMemo<string[]>(() => children.map((c) => c.props.name), [children]);
+	const tabs = useMemo<(string | undefined)[]>(() => children.map((c) => c?.props.name), [children]);
 
 	useLayoutEffect(() => {
 		if (children.length === 0)
 			return;
-		let defaultTab = children.findIndex((c) => c.props.default);
+		let defaultTab = children.findIndex((c) => c && c.props.default);
 		if (defaultTab < 0) {
 			defaultTab = 0;
 		}
@@ -32,7 +32,7 @@ export function TabContainer({ children, id, className }: {
 		<div className={ classNames('tab-container', className) } id={ id }>
 			<ul>
 				{
-					tabs.map((tab, index) => (
+					tabs.map((tab, index) => (tab &&
 						<li key={ index } className={ classNames('tab', { active: index === currentTab }) } onClick={ () => setTab(index) }>
 							{tab}
 						</li>

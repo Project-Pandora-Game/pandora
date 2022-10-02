1544db59664ebe104ff2f72ac782d53df5cf73dc
Sekkmer <sekkmer@gmail.com>
2022-10-02 20:03:20 +0200

    [FIX] test

--- pandora-server-directory/test/networking/connection_client.test.ts
+++ pandora-server-directory/test/networking/connection_client.test.ts
@@ -260,11 +260,15 @@ describe('ClientConnection', () => {
 			connectionOnMessage.mockClear();
 
 			client.setAccount(account);
+			expect(connectionOnMessage).toHaveBeenCalledTimes(1);
+			expect(connectionOnMessage).toHaveBeenNthCalledWith(1, 'directMessage', {
+				info: [],
+			}, undefined, expect.anything());
 
 			client.sendConnectionStateUpdate();
 
-			expect(connectionOnMessage).toHaveBeenCalledTimes(1);
-			expect(connectionOnMessage).toHaveBeenNthCalledWith(1, 'connectionState', {
+			expect(connectionOnMessage).toHaveBeenCalledTimes(2);
+			expect(connectionOnMessage).toHaveBeenNthCalledWith(2, 'connectionState', {
 				account: account.getAccountInfo(),
 				character: null,
 			}, undefined, expect.anything());

fb46676e1890bc08f3c0811c10d6582ba7c24d25
TechTheAwesome <techtheawesome01@gmail.com>
2022-10-08 00:50:38 +0900

    ADD viable muffling

--- pandora-common/src/chatroom/muffling.ts
+++ pandora-common/src/chatroom/muffling.ts
@@ -1,12 +1,20 @@
 import { PseudoRandom } from '../math/pseudoRandom';
 
+enum Difficulty {
+	HARDEST = 1,
+	HARD = 0.8,
+	NORMAL = 0.5,
+	EASY = 0.3,
+	NONE = 0,
+}
+
 type MufflerSetting = {
-	lipsTouch?: boolean,
-	jawMove?: boolean,
-	tongueRoof?: boolean,
-	mouthBlocked?: boolean,
-	throatBlocked?: boolean,
-	strength?: number,
+	lipsTouch?: Difficulty,
+	jawMove?: Difficulty,
+	tongueRoof?: Difficulty,
+	mouthBreath?: Difficulty,
+	throatBreath?: Difficulty,
+	coherency?: Difficulty,
 	stimulus?: number,
 };
 
@@ -14,9 +22,17 @@ export class Muffler {
 	private seed: string;
 	private setting: MufflerSetting;
 
-	constructor(seed: string, setting?: MufflerSetting) {
+	constructor(seed: string, setting?: Partial<MufflerSetting>) {
 		this.seed = seed;
-		this.setting = setting ?? {};
+		this.setting = setting ?? {
+			lipsTouch: Difficulty.EASY,
+			jawMove: Difficulty.EASY,
+			tongueRoof: Difficulty.EASY,
+			coherency: Difficulty.EASY,
+			mouthBreath: Difficulty.NONE,
+			throatBreath: Difficulty.NONE,
+			stimulus: Difficulty.NONE,
+		};
 	}
 
 	muffle(input: string): string {
@@ -29,37 +45,84 @@ export class Muffler {
 
 	private muffleWord(word: string): string {
 		const r = new PseudoRandom(word + this.seed);
-		const list: string[] = word.split('').map((c) => {
+		const { lipsTouch, jawMove, tongueRoof, coherency, mouthBreath, throatBreath } = this.setting;
+		let muffled: string[] = word.split('').map((c) => {
 			if (c.match(/[t]/ig)) {
-				return this.isUpper(c) ? this.roll(['T', 'Th', 'TH', 'Tph'], 0.5, r) ?? c : this.roll(['Th', 'th', 'tph'], 0.5, r) ?? c;
-			} else if (c.match(/[kqc]/ig)) {
-				return this.isUpper(c) ? this.roll(['Ch', 'Gh', 'gh'], 0.5, r) ?? c : this.roll(['ch', 'gh'], 0.5, r) ?? c;
-			} else if (c.match(/[gdbp]/ig)) {
-				return this.isUpper(c) ? this.roll(['Gh', 'gm'], 0.5, r) ?? c : this.roll(['ch', 'gh'], 0.5, r) ?? c;
+				return this.roll(['th', 'tph'], ['Th', 'TH', 'Tph'], tongueRoof ?? Difficulty.EASY, r, c);
+			} else if (c.match(/[kc]/ig)) {
+				return this.roll(['ch', 'gh'], ['Ch', 'Gh', 'gh'], tongueRoof ?? Difficulty.EASY, r, c);
+			} else if (c.match(/[z]/ig)) {
+				return this.roll(['gi'], ['Gi'], jawMove ?? 1, r, c);
+			} else if (c.match(/[bdgp]/ig)) {
+				return this.roll(['ch', 'gh'], ['Gh', 'Ch'], lipsTouch ?? Difficulty.EASY, r, c);
 			} else if (c.match(/[s]/ig)) {
-				return this.isUpper(c) ? this.roll(['Sh', 'sh'], 0.5, r) ?? c : 'sh';
+				return this.roll(['sh', 'ss'], ['Sh', 'Ss'], jawMove ?? Difficulty.EASY, r, c);
 			} else if (c.match(/[f]/ig)) {
-				return this.isUpper(c) ? this.roll(['Ph', 'ph'], 0.5, r) ?? c : 'ph';
-			} else if (c.match(/[lyr]/ig)) {
-				return this.isUpper(c) ? this.roll(['W'], 0.5, r) ?? c : this.roll(['w'], 0.5, r) ?? c;
+				return this.roll(['ph'], ['Ph'], lipsTouch ?? 1, r, c);
+			} else if (c.match(/[hjlrwxyq]/ig)) {
+				return this.roll(['w', 'm', 'h', 'n'], ['W', 'M', 'N'], coherency ?? 0, r, c);
 			} else if (c.match(/[aeiou]/ig)) {
-				return this.isUpper(c) ? this.roll(['M'], 0.5, r) ?? c : this.roll(['n'], 0.5, r) ?? c;
+				return this.roll(['m', 'n', 'w', 'h'], ['M', 'N', 'H', 'W'], jawMove ?? Difficulty.EASY, r, c);
 			} else {
 				return c;
 			}
 		});
 
-		return list.join('');
+		if (mouthBreath) {
+			muffled = muffled.map((c) => {
+				if (c.match(/(th|tph|ch|c)/ig)) {
+					return this.roll(['gh'], ['Gh'], mouthBreath, r, c);
+				} else if (c.match(/(ph)/ig)) {
+					return this.roll(['mh', 'nh'], ['Mh', 'Nh'], mouthBreath, r, c);
+				} else if (c.match(/(ss|sh)/ig)) {
+					return this.roll(['hh', 'hm'], ['Hh', 'Hm'], mouthBreath, r, c);
+				} else if (c.match(/[aeiou]/ig)) {
+					return this.roll(['m', 'n', 'w'], ['M', 'N', 'W'], mouthBreath, r, c);
+				} else if (c.match(/[hjlrvwxyq]/ig)) {
+					return this.roll(['w', 'm', 'n'], ['W', 'M', 'N'], mouthBreath, r, c);
+				} else if (c.match(/h/ig)) {
+					return this.roll(['m', 'n'], ['M', 'N'], mouthBreath, r, c);
+				} else if (c.match(/gi/ig)) {
+					return this.roll(['gm', 'gn'], ['Gm', 'Gn'], mouthBreath, r, c);
+				} else {
+					return c;
+				}
+			});
+		}
+
+		if (throatBreath) {
+			muffled = muffled.map((c) => {
+				if (c.match(/(mh|nh|hm|hh)/ig)) {
+					return this.roll(['mm', 'nn', 'mn', 'nm'], ['Mm', 'Nn', 'Mn', 'Nm'], throatBreath, r, c);
+				} else if (c.match(/gh|c|ch/ig)) {
+					return this.roll(['gm', 'gn'], ['Gm', 'Gn'], throatBreath, r, c);
+				} else if (c.match(/w/ig)) {
+					return this.roll(['m', 'n'], ['M', 'N'], throatBreath, r, c);
+				} else if (c.match(/[aeiou]/ig)) {
+					return this.roll(['m', 'n'], ['M', 'N'], throatBreath, r, c);
+				} else if (c.match(/[hjlrvwxyq]/ig)) {
+					return this.roll(['m', 'n'], ['M', 'N'], throatBreath, r, c);
+				} else if (c.match(/h/ig)) {
+					return this.roll(['m', 'n'], ['M', 'N'], throatBreath, r, c);
+				} else if (c.match(/gi/ig)) {
+					return this.roll(['gm', 'gn'], ['Gm', 'Gn'], throatBreath, r, c);
+				} else {
+					return c;
+				}
+			});
+		}
+
+		return muffled.join('').replace(/hh/ig, 'h').replace(/ww/ig, 'w');
 	}
 
 	private isUpper(char: string) {
-		return char === char.toUpperCase();
+		return char.charAt(0) === char.charAt(0).toUpperCase();
 	}
 
-	private roll(muf: string[], probMuf: number, random: PseudoRandom): string | null {
+	private roll(muf: string[], mufUpper: string[], probMuf: number, random: PseudoRandom, c: string): string {
 		if (random.rand() <= probMuf) {
-			return muf[Math.round(random.between(0, muf.length - 1))];
+			return this.isUpper(c) ? mufUpper[Math.round(random.between(0, mufUpper.length - 1))] : muf[Math.round(random.between(0, muf.length - 1))];
 		}
-		return null;
+		return c;
 	}
 }
--- pandora-common/test/chatroom/muffling.test.ts
+++ pandora-common/test/chatroom/muffling.test.ts
@@ -14,7 +14,26 @@ describe('Muffler', () => {
 			const test = 'The quick brown fox jumps over the lazy dog';
 			expect(muffler.muffle(test.toUpperCase()).split('').every((c) => c === c.toUpperCase()));
 			expect(muffler.muffle(test.toLowerCase()).split('').every((c) => c === c.toLowerCase()));
+			// eslint-disable-next-line no-console
 			console.log(muffler.muffle(test));
+		});
+
+		it('should be word consistent', () => {
+			const muffler = new Muffler('character second name');
+			const test1 = 'The quick brown fox jumps over the lazy dog';
+			const test2 = 'The quick brown fox jumps over the lazy dog, so she does!';
+			expect(muffler.muffle(test1)).toBe(muffler.muffle(test1));
+			expect(muffler.muffle(test2).includes(muffler.muffle(test1)));
+
+		});
+
+		it('should keep punctuations', () => {
+			const muffler = new Muffler('character second name ewqlkhrql;kwehrkjq');
+			expect(muffler.muffle('Hello!').includes('!')).toBeTruthy();
+			expect(muffler.muffle('I\'m batman').includes('\'')).toBeTruthy();
+			expect(muffler.muffle('How are you?').includes('?')).toBeTruthy();
+			expect(muffler.muffle('Nice to meet you.').includes('.')).toBeTruthy();
+			expect(muffler.muffle('What a nice day, let\'s go swimming').includes(',')).toBeTruthy();
 		});
 	});
 

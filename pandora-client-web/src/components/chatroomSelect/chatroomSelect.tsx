97a6709aab020227d43278db6a4d2f2a7c93be8f
SandrinePDR <sandrine.bclub@gmail.com>
2022-11-28 13:42:02 +0100

    Intermediate version, working on the room details

--- pandora-client-web/src/components/chatroomSelect/chatroomSelect.tsx
+++ pandora-client-web/src/components/chatroomSelect/chatroomSelect.tsx
@@ -1,13 +1,15 @@
-import { noop } from 'lodash';
-import { EMPTY, GetLogger, IChatRoomDirectoryInfo, IClientDirectoryNormalResult, RoomId } from 'pandora-common';
-import React, { ReactElement, useCallback, useState } from 'react';
+import { identity, noop } from 'lodash';
+import { ChatRoomBaseInfoSchema, EMPTY, GetLogger, IChatRoomDirectoryInfo, IClientDirectoryNormalResult, RoomId } from 'pandora-common';
+import React, { Children, ReactElement, useCallback, useState } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { useErrorHandler } from '../../common/useErrorHandler';
 import { PersistentToast } from '../../persistentToast';
 import { Button } from '../common/Button/Button';
-import { useChatRoomData } from '../gameContext/chatRoomContextProvider';
+import { useChatRoomData, useChatRoomCharacters } from '../gameContext/chatRoomContextProvider';
 import { useDirectoryChangeListener, useDirectoryConnector } from '../gameContext/directoryConnectorContextProvider';
 import { useConnectToShard } from '../gameContext/shardConnectorContextProvider';
+// import { useChatRoomData, useChatRoomCharacters, useCharacterRestrictionsManager } from '../gameContext/chatRoomContextProvider';
+import { Dialog } from '../dialog/dialog';
 import './chatroomSelect.scss';
 import closedDoor from './closed-door.svg';
 import openDoor from './opened-door.svg';
@@ -17,19 +19,22 @@ export function ChatroomSelect(): ReactElement {
 	const roomData = useChatRoomData();
 	const roomList = useRoomList();
 
+	/*
 	if (roomData) {
 		return <Navigate to='/chatroom' />;
 	}
-
+	*/
 	return (
 		<div>
 			<Link to='/pandora_lobby'>◄ Back to lobby</Link><br />
 			<Button onClick={ () => navigate('/chatroom_create') }>Create room</Button><br />
 			<p>
-				Existing rooms:<br />
-				{ !roomList ? <div className='loading'>Loading...</div> : (
-					roomList.map((room) => <RoomEntry key={ room.id } { ...room } />)
-					)}
+				Existing rooms: {roomList?.length }<br />
+				{!roomList ? <div className='loading'>Loading...</div> : (
+					roomList.length > 0 ?
+						roomList.map((room) => <RoomEntry key={room.id} {...room} />) :
+						<div>No room matches your filter criteria</div>
+				)}
 			</p>
 		</div>
 	);
@@ -37,12 +42,33 @@ export function ChatroomSelect(): ReactElement {
 
 function RoomEntry({ id, name, description: _description, hasPassword: _hasPassword, maxUsers, users, protected: _roomIsProtected }: IChatRoomDirectoryInfo): ReactElement {
 	const joinRoom = useJoinRoom();
+	const roomDetails = useRoomList()?.find(element => element.id == id);
+//	const roomDetails = useChatRoomData();
+	const characters = useChatRoomCharacters();
+
+	const [show, setShow] = useState(false);
 	return (
-		<a className='room-list-grid' onClick={ () => void joinRoom(id) }>
-			<img className='room-list-entry' width='50px' height='50px' src={ _roomIsProtected ? closedDoor : openDoor } alt={ _roomIsProtected ? 'Protected room' : 'Open room' }></img>
+		<a className='room-list-grid' onClick={() => void setShow(true)} >
+			<img className='room-list-entry' width='50px' src={ _roomIsProtected ? closedDoor : openDoor } alt={ _roomIsProtected ? 'Protected room' : 'Open room' }></img>
 			<div className='room-list-entry'>{`${name} (${users}/${maxUsers})`}</div>
 			<div className='room-list-entry'>Some icons</div>
-			<div className='room-list-entry'>{(_description.length > 50) ? `${_description.substring(0, 47).concat('...') }` : `${_description}`}</div>
+			<div className='room-list-entry'>{(_description.length > 50) ? `${_description.substring(0, 47).concat('\u2026') }` : `${_description}`}</div>
+			{show && (
+				<Dialog>
+					<p>
+						<div>Details for room {roomDetails?.name}</div>
+						<div>Description:</div>
+						<div>{roomDetails?.description}</div>
+						<div>Current users in this room ({roomDetails?.users}/{characters?.length }):
+							<ul>
+								{characters?.map((char) => <li>{char.data.name}</li>)}
+							</ul>
+						</div>
+					</p>
+					<Button className='slim' onClick={() => void joinRoom}>Enter Room</Button>
+					<Button className='slim' onClick={() => void close}>Close</Button>
+				</Dialog>
+			)}
 		</a>
 	);
 }

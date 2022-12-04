ff86ea06ea06f28ff244972eb8f4a4b7dfc521a4
Sekkmer <sekkmer@gmail.com>
2022-12-04 15:49:44 +0100

    [ADD] UI to set character pronoun

--- pandora-client-web/src/components/settings/characterSettings.tsx
+++ pandora-client-web/src/components/settings/characterSettings.tsx
@@ -4,6 +4,7 @@ import { Button } from '../common/Button/Button';
 import { usePlayerData } from '../gameContext/playerContextProvider';
 import { useShardConnector } from '../gameContext/shardConnectorContextProvider';
 import { ColorInput, useColorInput } from '../common/colorInput/colorInput';
+import { PronounKey, PRONOUNS } from 'pandora-common/dist/character/pronouns';
 
 export function CharacterSettings(): ReactElement | null {
 	const playerData = usePlayerData();
@@ -12,7 +13,10 @@ export function CharacterSettings(): ReactElement | null {
 		return <>No character selected</>;
 
 	return (
-		<LabelColor playerData={ playerData } />
+		<>
+			<LabelColor playerData={ playerData } />
+			<Pronouns playerData={ playerData } />
+		</>
 	);
 }
 
@@ -30,6 +34,33 @@ function LabelColor({ playerData }: { playerData: Readonly<ICharacterData> }): R
 					className='slim'
 					onClick={ () => shardConnector?.sendMessage('updateSettings', { labelColor: color }) }
 					disabled={ color === playerData.settings.labelColor?.toUpperCase() }>
+					Save
+				</Button>
+			</div>
+		</fieldset>
+	);
+}
+
+function Pronouns({ playerData }: { playerData: Readonly<ICharacterData> }): ReactElement {
+	const shardConnector = useShardConnector();
+	const [pronoun, setProunoun] = React.useState(playerData.settings.pronoun);
+
+	return (
+		<fieldset>
+			<legend>Pronouns</legend>
+			<div className='input-row'>
+				<label>Pronoun</label>
+				<select value={ pronoun } onChange={ (ev) => setProunoun(ev.target.value as PronounKey) }>
+					{ Object.entries(PRONOUNS).map(([key, value]) => (
+						<option key={ key } value={ key }>
+							{ Object.values(value).join('/') }
+						</option>
+					)) }
+				</select>
+				<Button
+					className='slim'
+					onClick={ () => shardConnector?.sendMessage('updateSettings', { pronoun }) }
+					disabled={ pronoun === playerData.settings.pronoun }>
 					Save
 				</Button>
 			</div>

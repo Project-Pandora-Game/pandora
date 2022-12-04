6c3f73d66628af72281c5107337ba19ba964e4a8
Jomshir98 <jomshir98@protonmail.com>
2022-12-04 15:54:28 +0000

    [FIX] Use pointer events for hybrid devices

--- pandora-client-web/src/components/hoverElement/hoverElement.tsx
+++ pandora-client-web/src/components/hoverElement/hoverElement.tsx
@@ -8,8 +8,6 @@ type HoverElementProps = CommonProps & {
 	parent: RefObject<HTMLElement>;
 };
 
-const IS_MOUSE_DETECTED = window.matchMedia('(hover: hover)').matches;
-
 export function HoverElement({ children, className, parent }: HoverElementProps): ReactElement | null {
 	const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
 	const [show, setShow] = useState(false);
@@ -20,42 +18,21 @@ export function HoverElement({ children, className, parent }: HoverElementProps)
 
 		const current = parent.current;
 
-		if (!IS_MOUSE_DETECTED) {
-			const hover = (event: TouchEvent) => {
-				setAnchorPoint({ x: event.touches[0].pageX, y: event.touches[0].pageY - 32 });
-				setShow(true);
-			};
-			const end = (_event: TouchEvent) => {
-				setShow(false);
-			};
-			current.addEventListener('touchstart', hover);
-			current.addEventListener('touchmove', hover);
-			current.addEventListener('touchend', end);
-			return () => {
-				current.removeEventListener('touchstart', hover);
-				current.addEventListener('touchmove', hover);
-				current.removeEventListener('touchend', end);
-			};
-		} else {
-			const hover = (event: MouseEvent) => {
-				event.stopPropagation();
-				event.preventDefault();
-				setAnchorPoint({ x: event.pageX, y: event.pageY - 16 });
-				setShow(true);
-			};
-			const end = (event: MouseEvent) => {
-				event.preventDefault();
-				setShow(false);
-			};
-			current.addEventListener('mouseenter', hover);
-			current.addEventListener('mousemove', hover);
-			current.addEventListener('mouseleave', end);
-			return () => {
-				current.removeEventListener('mouseenter', hover);
-				current.removeEventListener('mousemove', hover);
-				current.removeEventListener('mouseleave', end);
-			};
-		}
+		const hover = (event: PointerEvent) => {
+			setAnchorPoint({ x: event.pageX, y: event.pageY - (event.pointerType === 'mouse' ? 16 : 32) });
+			setShow(true);
+		};
+		const end = (_event: PointerEvent) => {
+			setShow(false);
+		};
+		current.addEventListener('pointerover', hover);
+		current.addEventListener('pointermove', hover);
+		current.addEventListener('pointerleave', end);
+		return () => {
+			current.removeEventListener('pointerover', hover);
+			current.removeEventListener('pointermove', hover);
+			current.removeEventListener('pointerleave', end);
+		};
 	}, [parent]);
 
 	const positionRef = useContextMenuPosition(anchorPoint, {

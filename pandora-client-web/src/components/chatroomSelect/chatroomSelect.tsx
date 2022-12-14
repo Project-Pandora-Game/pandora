3ace9f47896482dfecc6186f96e7908e2c4f6e08
SandrinePDR <sandrine.bclub@gmail.com>
2022-12-14 22:18:10 +0100

    [CHANGE] Added the review comments

--- pandora-client-web/src/components/chatroomSelect/chatroomSelect.tsx
+++ pandora-client-web/src/components/chatroomSelect/chatroomSelect.tsx
@@ -52,11 +52,13 @@ function RoomEntry({ roomInfo }: {
 
 	return (
 		<>
-			<a className='room-list-grid' onClick={ () => setShow(true) } >
-				<img className='room-list-entry' width='50px' src={ roomIsProtected ? closedDoor : openDoor } title={ roomIsProtected ? 'Protected room' : 'Open room' }></img>
-				<div className='room-list-entry'>{`${name} (${users}/${maxUsers})`}</div>
-				<div className='room-list-entry'></div>
-				<div className='room-list-entry'>{(description.length > 50) ? `${description.substring(0, 47).concat('\u2026')}` : `${description}`}</div>
+			<a className='roomListGrid' onClick={ () => setShow(true) } >
+				<img className='roomListGrid entry' width='50px'
+					src={ roomIsProtected ? closedDoor : openDoor }
+					title={ roomIsProtected ? 'Protected room' : 'Open room' }
+					alt={ roomIsProtected ? 'Protected room' : 'Open room' } />
+				<div className='roomListGrid entry'>{`${name} (${users}/${maxUsers})`}</div>
+				<div className='roomListGrid entry'>{(description.length > 50) ? `${description.substring(0, 49).concat('\u2026')}` : `${description}`}</div>
 			</a>
 			{ show && <RoomDetailsDialog
 				baseRoomInfo={ roomInfo }
@@ -70,6 +72,7 @@ function RoomDetailsDialog({ baseRoomInfo, hide }: {
 	baseRoomInfo: IChatRoomDirectoryInfo
 	hide: () => void;
 }): ReactElement | null {
+
 	const accountId = useCurrentAccount()?.id;
 	const [roomPassword, setPassword] = useState('');
 	const joinRoom = useJoinRoom();
@@ -100,44 +103,45 @@ function RoomDetailsDialog({ baseRoomInfo, hide }: {
 
 	return (
 		<ModalDialog>
-			<div className='chatroom-details'>
+			<div className='chatroomDetails'>
 				<div>Details for room<br /> <b>{name}</b></div>
-				{(background !== '' && !background.startsWith('#')) && <img className='details-preview' src={ background } width='200px' height='100px' ></img>}
-				<div className='details-features'>
-					{ roomIsProtected && <img className='details-features-img' src={ closedDoor } title='Protected Room' /> }
-					{ CHATROOM_FEATURES.map((f) => <div key={ f.id }>{ features.includes(f.id) && <img className='details-features-img' src={ f.icon } title={ f.name } /> }</div>) }
+				{ (background !== '' && !background.startsWith('#')) && <img className='chatroomDetails preview' src={ background } width='200px' height='100px' />}
+				<div className='chatroomDetails features'>
+					{ roomIsProtected && <img className='chatroomDetails features-img' src={ closedDoor } title='Protected Room' /> }
+					{ CHATROOM_FEATURES.map((f) => <div key={ f.id }>{ features.includes(f.id) && <img className='chatroomDetails features-img' src={ f.icon } title={ f.name } alt={ f.name } /> }</div>) }
 				</div>
-				<div className='details-description-title'>Description:</div>
-				<textarea className='details-widebox' value={ description } rows={ 20 } readOnly />
+				<div className='chatroomDetails description-title'>Description:</div>
+				<textarea className='chatroomDetails widebox' value={ description } rows={ 10 } readOnly />
 				{characters.length > 0 &&
-					<div className='details-title'>Current users in this room:
-						<div className='details-users-list'>
+					<div className='chatroomDetails title'>Current users in this room:
+						<div className='chatroomDetails users-list'>
 							{characters.map((char) => <div key={ char.id }>{char.name} ({char.id})</div>)}
 						</div>
 					</div>}
 				{(!userIsAdmin && roomIsProtected && hasPassword) &&
-					<div className='details-title'>This room requires a password:</div>}
+					<div className='chatroomDetails title'>This room requires a password:</div>}
 				{(!userIsAdmin && roomIsProtected && hasPassword) &&
-					<input
-						className='details-widebox'
+					<input className='chatroomDetails widebox'
 						name='roomPwd'
 						type='password'
 						value={ roomPassword }
 						onChange={ (e) => setPassword(e.target.value) }
 					/>}
-				<Row className='details-buttons' alignX='end'>
+				<Row className='chatroomDetails buttons' alignX='end'>
 					<Button className='slim' onClick={ hide }>Close</Button>
-					<Button className='slim' onClick={ () => {
-						joinRoom(id, roomPassword)
-							.then((joinResult) => {
-								if (joinResult === 'notFound')
-									hide();
-								// For any other reason of failed join we more likely want to somehow show a message
-							})
-							.catch((_error: unknown) => {
-								// You can handle if joining crashed or server communication failed here
-							});
-					} }>
+					<Button className='slim'
+						disabled={ ((!userIsAdmin && roomIsProtected && !hasPassword) || (!userIsAdmin && roomIsProtected && roomPassword.length === 0)) ? true : false }
+						onClick={ () => {
+							joinRoom(id, roomPassword)
+								.then((joinResult) => {
+									if (joinResult === 'notFound')
+										hide();
+									// For any other reason of failed join we more likely want to somehow show a message
+								})
+								.catch((_error: unknown) => {
+									// You can handle if joining crashed or server communication failed here
+								});
+						} }>
 						Enter Room
 					</Button>
 				</Row>

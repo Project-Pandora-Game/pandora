c7c73598ac51b0dd5a815da22c19798084921812
Sekkmer <sekkmer@gmail.com>
2023-02-27 02:51:33 +0100

    [FIX] build

--- pandora-client-web/src/editor/graphics/character/appearanceEditor.ts
+++ pandora-client-web/src/editor/graphics/character/appearanceEditor.ts
@@ -74,7 +74,8 @@ export class EditorCharacter extends TypedEventEmitter<AppearanceEvents> impleme
 
 	constructor() {
 		super();
-		this.appearance = new AppearanceEditor(GetAssetManagerEditor(), { id: this.id, accountId: 0, name: 'EditorCharacter' }, (changes) => this.emit('appearanceUpdate', changes));
+		const data = { id: this.id, accountId: 0, name: 'EditorCharacter' } as const;
+		this.appearance = new AppearanceEditor(GetAssetManagerEditor(), () => data, (changes) => this.emit('appearanceUpdate', changes));
 	}
 
 	public getRestrictionManager(roomContext: ActionRoomContext | null): CharacterRestrictionsManager {

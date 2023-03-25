7501c9c3326e635c8f374f286a0a90b4b2b8e9d6
Jomshir98 <jomshir98@protonmail.com>
2023-03-25 09:37:59 +0100

    [FIX] Clear room's pending messages on room disconnect

--- pandora-server-directory/src/room/room.ts
+++ pandora-server-directory/src/room/room.ts
@@ -443,6 +443,8 @@ export class Room {
 
 	public disconnect(): void {
 		this.assignedShard?.disconnectRoom(this);
+		// Clear pending action messages when room gets disconnected (this is no triggered on simple reassignment)
+		this.pendingMessages.length = 0;
 	}
 
 	public async generateAccessId(): Promise<string | null> {

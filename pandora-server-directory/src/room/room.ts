79d3ab433e9b7bb239548c87f7c3a63cb2763fad
Jomshir98 <jomshir98@protonmail.com>
2023-03-26 14:34:05 +0200

    [DEV] Review changes
    Co-authored-by: Sekkmer <sekkmer@gmail.com>

--- pandora-server-directory/src/room/room.ts
+++ pandora-server-directory/src/room/room.ts
@@ -443,7 +443,7 @@ export class Room {
 
 	public disconnect(): void {
 		this.assignedShard?.disconnectRoom(this);
-		// Clear pending action messages when room gets disconnected (this is no triggered on simple reassignment)
+		// Clear pending action messages when the room gets disconnected (this is not triggered on simple reassignment)
 		this.pendingMessages.length = 0;
 	}
 

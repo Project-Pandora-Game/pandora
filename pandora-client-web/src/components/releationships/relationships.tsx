f9105f2fcf969f8f9881671693defda6a436054d
Sekkmer <sekkmer@gmail.com>
2023-04-06 02:14:53 +0200

    [ADD] unchecked friend actions

--- pandora-client-web/src/components/releationships/relationships.tsx
+++ pandora-client-web/src/components/releationships/relationships.tsx
@@ -6,6 +6,8 @@ import { Column, Row } from '../common/container/container';
 import { Tab, TabContainer } from '../common/tabs/tabs';
 import { DirectMessages } from '../directMessages/directMessages';
 import './relationships.scss';
+import { Button } from '../common/button/button';
+import { useDirectoryConnector } from '../gameContext/directoryConnectorContextProvider';
 
 export const RELATIONSHIPS = new Observable<readonly IAccountRelationship[]>([]);
 export const FRIEND_STATUS = new Observable<readonly IAccountFriendStatus[]>([]);
@@ -50,6 +52,7 @@ function ShowRelationships({ type }: { type: IAccountRelationship['type']; }) {
 				<span>ID</span>
 				<span>Name</span>
 				<span>Created</span>
+				<span>Actions</span>
 			</Row>
 			{ rel.map((rel) => (
 				<RelationshipsRow key={ rel.id } { ...rel } />
@@ -62,16 +65,49 @@ function RelationshipsRow({
 	id,
 	name,
 	time,
+	type,
 }: {
 	id: AccountId;
 	name: string;
 	time: number;
+	type: IAccountRelationship['type'];
 }) {
+	const directory = useDirectoryConnector();
+	const actions = useMemo(() => {
+		switch (type) {
+			case 'blocked':
+				return (
+					<Button className='slim' onClick={
+						() => directory.sendMessage('blockList', { id, action: 'remove' })
+					}>Unblock</Button>
+				);
+			case 'pending':
+				return (
+					<Button className='slim' onClick={
+						() => void directory.awaitResponse('friendRequest', { id, action: 'cancel' }).catch(() => { /* ignore */ })
+					}>Cancel</Button>
+				);
+			case 'incoming':
+				return (
+					<>
+						<Button className='slim' onClick={
+							() => void directory.awaitResponse('friendRequest', { id, action: 'accept' }).catch(() => { /* ignore */ })
+						}>Accept</Button>
+						<Button className='slim' onClick={
+							() => void directory.awaitResponse('friendRequest', { id, action: 'decline' }).catch(() => { /* ignore */ })
+						}>Decline</Button>
+					</>
+				);
+			default:
+				return null;
+		}
+	}, [type, name, id, directory]);
 	return (
 		<Row>
 			<span>{ id }</span>
 			<span>{ name }</span>
 			<span>{ new Date(time).toLocaleString() }</span>
+			<span>{ actions }</span>
 		</Row>
 	);
 }
@@ -100,6 +136,7 @@ function ShowFriends() {
 				<span>Created</span>
 				<span>Status</span>
 				<span>Online Characters</span>
+				<span>Actions</span>
 			</Row>
 			{ friendsWithStatus.map((friend) => (
 				<FriendRow key={ friend.id } { ...friend } />
@@ -121,6 +158,7 @@ function FriendRow({
 	status: string;
 	characters?: IAccountFriendStatus['characters'];
 }) {
+	const directory = useDirectoryConnector();
 	return (
 		<Row>
 			<span>{ id }</span>
@@ -128,6 +166,11 @@ function FriendRow({
 			<span>{ new Date(time).toLocaleString() }</span>
 			<span>{ status }</span>
 			<span>{ characters?.length !== 0 ? 'yes' : 'no' }</span>
+			<span>
+				<Button className='slim' onClick={
+					() => void directory.awaitResponse('unfriend', { id }).catch(() => { /* ignore */ })
+				}>Remove</Button>
+			</span>
 		</Row>
 	);
 }

89ff2aa1642deed5e020b3b86626b2db9b9a3dce
Sekkmer <sekkmer@gmail.com>
2023-04-15 17:44:11 +0200

    [ADD] token info to shard connections

--- pandora-server-directory/src/networking/connection_shard.ts
+++ pandora-server-directory/src/networking/connection_shard.ts
@@ -3,20 +3,24 @@ import { ConnectionType, IConnectionShard } from './common';
 import { ConnectionManagerShard } from './manager_shard';
 import { Shard } from '../shard/shard';
 import { SocketInterfaceRequest, SocketInterfaceResponse } from 'pandora-common/dist/networking/helpers';
+import type { ConnectedTokenInfo, IConnectedTokenInfo } from '../shard/shardTokenStore';
 
 /** Class housing connection from a shard */
 export class ShardConnection extends IncomingConnection<IDirectoryShard, IShardDirectory, IncomingSocket> implements IConnectionShard {
 	public readonly type: ConnectionType.SHARD = ConnectionType.SHARD;
+	readonly info: ConnectedTokenInfo;
 
 	public shard: Shard | null = null;
 
-	constructor(server: IServerSocket<IDirectoryShard>, socket: IncomingSocket) {
+	constructor(server: IServerSocket<IDirectoryShard>, socket: IncomingSocket, info: ConnectedTokenInfo) {
 		super(server, socket, [DirectoryShardSchema, ShardDirectorySchema], GetLogger('Connection-Shard', `[Connection-Shard ${socket.id}]`));
+		this.info = info;
 		this.logger.verbose('Connected');
 	}
 
 	protected override onDisconnect(reason: string): void {
 		this.logger.verbose('Disconnected, reason:', reason);
+		this.info.remove();
 		ConnectionManagerShard.onDisconnect(this);
 	}
 
@@ -25,5 +29,9 @@ export class ShardConnection extends IncomingConnection<IDirectoryShard, IShardD
 		message: SocketInterfaceRequest<IShardDirectory>[K],
 	): Promise<SocketInterfaceResponse<IShardDirectory>[K]> {
 		return ConnectionManagerShard.onMessage(messageType, message, this);
+	}
+
+	public getTokenInfo(): Readonly<IConnectedTokenInfo> {
+		return this.info;
 	}
 }

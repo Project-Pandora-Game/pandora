8a6ef394049d957a1ad630db2bf3e5559c173a03
Sekkmer <sekkmer@gmail.com>
2023-05-27 00:52:09 +0200

    [FEATURE] device context menu (#252)

--- pandora-client-web/src/components/chatroom/chatRoomScene.tsx
+++ pandora-client-web/src/components/chatroom/chatRoomScene.tsx
@@ -2,29 +2,27 @@ import { AssertNotNullable, CalculateCharacterMaxYForBackground, CharacterId, EM
 import * as PIXI from 'pixi.js';
 import { FederatedPointerEvent, Filter, Rectangle } from 'pixi.js';
 import { Container, Graphics } from '@pixi/react';
-import React, { ReactElement, useCallback, useEffect, useMemo, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { ReactElement, useCallback, useMemo, useState } from 'react';
 import { useEvent } from '../../common/useEvent';
 import { Character, useCharacterData } from '../../character/character';
 import { ShardConnector } from '../../networking/shardConnector';
-import { useChatRoomInfo, useChatRoomCharacters, useCharacterRestrictionsManager, IsChatroomAdmin, ChatRoom, useCharacterState } from '../gameContext/chatRoomContextProvider';
+import { useChatRoomInfo, useChatRoomCharacters, useCharacterRestrictionsManager, ChatRoom, useCharacterState } from '../gameContext/chatRoomContextProvider';
 import { useShardConnector } from '../gameContext/shardConnectorContextProvider';
-import { useChatInput } from './chatInput';
-import { usePlayer, usePlayerId } from '../gameContext/playerContextProvider';
+import { usePlayer } from '../gameContext/playerContextProvider';
 import { IBounceOptions } from 'pixi-viewport';
 import { CommonProps } from '../../common/reactTypes';
 import { useAssetManager, GetAssetsSourceUrl } from '../../assets/assetManager';
 import { ChatroomDebugConfig, useDebugConfig } from './chatroomDebug';
-import { useContextMenuPosition } from '../contextMenu/contextMenu';
 import { PixiViewportSetupCallback } from '../../graphics/pixiViewport';
 import { GraphicsScene, GraphicsSceneProps } from '../../graphics/graphicsScene';
 import { ChatRoomCharacter } from './chatRoomCharacter';
-import { useCurrentAccount, useDirectoryConnector } from '../gameContext/directoryConnectorContextProvider';
 import { PointLike } from '../../graphics/graphicsCharacter';
 import { ChatRoomDevice } from './chatRoomDevice';
 import { useChatroomRequired } from '../gameContext/chatRoomContextProvider';
 import { useRoomInventoryItems } from '../gameContext/chatRoomContextProvider';
 import { shardConnectorContext } from '../gameContext/shardConnectorContextProvider';
+import { DeviceContextMenu } from './contextMenus/deviceContextMenu';
+import { CharacterContextMenu } from './contextMenus/characterContextMenu';
 
 const BONCE_OVERFLOW = 500;
 const BASE_BOUNCE_OPTIONS: IBounceOptions = {
@@ -45,7 +43,7 @@ interface ChatRoomGraphicsSceneProps extends CommonProps {
 	filters: PIXI.Filter[];
 	filtersExclude?: readonly (CharacterId | ItemId)[];
 	onPointerDown: (event: React.PointerEvent<HTMLDivElement>) => void;
-	menuOpen: (character: Character<ICharacterRoomData>, event: FederatedPointerEvent) => void;
+	menuOpen: (target: Character<ICharacterRoomData> | ItemRoomDevice, event: FederatedPointerEvent) => void;
 }
 
 export function ChatRoomGraphicsScene({
@@ -151,6 +149,7 @@ export function ChatRoomGraphicsScene({
 							debugConfig={ debugConfig }
 							background={ roomBackground }
 							shard={ shard }
+							menuOpen={ menuOpen }
 							filters={ filtersExclude.includes(device.id) ? EMPTY_ARRAY : filters }
 						/>
 					) : null))
@@ -173,7 +172,11 @@ export function ChatRoomScene(): ReactElement | null {
 	const info = useChatRoomInfo();
 	const characters = useChatRoomCharacters();
 	const shard = useShardConnector();
-	const [menuActive, setMenuActive] = useState<{ character: Character<ICharacterRoomData>; position: Readonly<PointLike>; } | null>(null);
+	const [menuActive, setMenuActive] = useState<{
+		character?: Character<ICharacterRoomData>;
+		deviceItemId?: ItemId;
+		position: Readonly<PointLike>;
+	} | null>(null);
 	const player = usePlayer();
 	const debugConfig = useDebugConfig();
 
@@ -189,12 +192,13 @@ export function ChatRoomScene(): ReactElement | null {
 
 	const blindness = useCharacterRestrictionsManager(playerState, player, (manager) => manager.getBlindness());
 
-	const menuOpen = useCallback((character: Character<ICharacterRoomData> | null, event: FederatedPointerEvent | null) => {
-		if (!character || !event) {
+	const menuOpen = useCallback((target: Character<ICharacterRoomData> | ItemRoomDevice | null, event: FederatedPointerEvent | null) => {
+		if (!target || !event) {
 			setMenuActive(null);
 		} else {
 			setMenuActive({
-				character,
+				character: target instanceof Character ? target : undefined,
+				deviceItemId: target instanceof ItemRoomDevice ? target.id : undefined,
 				position: {
 					x: event.pageX,
 					y: event.pageY,
@@ -243,131 +247,11 @@ export function ChatRoomScene(): ReactElement | null {
 			menuOpen={ menuOpen }
 		>
 			{
-				menuActive ? <CharacterContextMenu character={ menuActive.character } position={ menuActive.position } onClose={ closeContextMenu } /> : null
+				menuActive?.character ? <CharacterContextMenu character={ menuActive.character } position={ menuActive.position } onClose={ closeContextMenu } /> : null
+			}
+			{
+				menuActive?.deviceItemId ? <DeviceContextMenu deviceItemId={ menuActive.deviceItemId } position={ menuActive.position } onClose={ closeContextMenu } /> : null
 			}
 		</ChatRoomGraphicsScene>
-	);
-}
-
-function AdminActionContextMenu({ character, chatRoomInfo, onClose, onBack }: { character: Character<ICharacterRoomData>; chatRoomInfo: IChatRoomFullInfo; onClose: () => void; onBack: () => void; }): ReactElement {
-	const isCharacterAdmin = IsChatroomAdmin(chatRoomInfo, { id: character.data.accountId });
-	const connector = useDirectoryConnector();
-
-	const kick = useCallback(() => {
-		connector.sendMessage('chatRoomAdminAction', { action: 'kick', targets: [character.data.accountId] });
-		onClose();
-	}, [character, connector, onClose]);
-
-	const ban = useCallback(() => {
-		connector.sendMessage('chatRoomAdminAction', { action: 'ban', targets: [character.data.accountId] });
-		onClose();
-	}, [character, connector, onClose]);
-
-	const promote = useCallback(() => {
-		connector.sendMessage('chatRoomAdminAction', { action: 'promote', targets: [character.data.accountId] });
-		onClose();
-	}, [character, connector, onClose]);
-
-	const demote = useCallback(() => {
-		connector.sendMessage('chatRoomAdminAction', { action: 'demote', targets: [character.data.accountId] });
-		onClose();
-	}, [character, connector, onClose]);
-
-	return (
-		<>
-			<button onClick={ kick } >
-				Kick
-			</button>
-			<button onClick={ ban } >
-				Ban
-			</button>
-			{ isCharacterAdmin ? (
-				<button onClick={ demote } >
-					Demote
-				</button>
-			) : (
-				<button onClick={ promote } >
-					Promote
-				</button>
-			) }
-			<button onClick={ onBack } >
-				Back
-			</button>
-		</>
-	);
-}
-
-function CharacterContextMenu({ character, position, onClose }: {
-	character: Character<ICharacterRoomData>;
-	position: Readonly<PointLike>;
-	onClose: () => void;
-}): ReactElement | null {
-	const navigate = useNavigate();
-	const { setTarget } = useChatInput();
-	const playerId = usePlayerId();
-	const currentAccount = useCurrentAccount();
-	const [menu, setMenu] = useState<'main' | 'admin'>('main');
-
-	const ref = useContextMenuPosition(position);
-
-	const characterData = useCharacterData(character);
-	const chatRoomInfo = useChatRoomInfo();
-	const isPlayerAdmin = IsChatroomAdmin(chatRoomInfo, currentAccount);
-
-	useEffect(() => {
-		if (!isPlayerAdmin && menu === 'admin') {
-			setMenu('main');
-		}
-	}, [isPlayerAdmin, menu]);
-
-	const onCloseActual = useCallback(() => {
-		setMenu('main');
-		onClose();
-	}, [onClose]);
-
-	if (!event || !chatRoomInfo) {
-		return null;
-	}
-
-	return (
-		<div className='context-menu' ref={ ref } onPointerDown={ (e) => e.stopPropagation() }>
-			<span>
-				{ characterData.name } ({ characterData.id })
-			</span>
-			{ menu === 'main' && (
-				<>
-					<button onClick={ () => {
-						onCloseActual();
-						navigate('/wardrobe', { state: { character: characterData.id } });
-					} }>
-						Wardrobe
-					</button>
-					{ characterData.id !== playerId && (
-						<button onClick={ () => {
-							onClose();
-							setTarget(characterData.id);
-						} }>
-							Whisper
-						</button>
-					) }
-				</>
-			) }
-			{ (isPlayerAdmin && character.data.accountId !== currentAccount?.id) && (
-				<>
-					{ menu === 'main' ? (
-						<button onClick={ () => setMenu('admin') }>
-							Admin
-						</button>
-					) : (
-						<AdminActionContextMenu character={ character } chatRoomInfo={ chatRoomInfo } onClose={ onCloseActual } onBack={ () => setMenu('main') } />
-					) }
-				</>
-			) }
-			<button onClick={ () => {
-				onCloseActual();
-			} } >
-				Close
-			</button>
-		</div>
 	);
 }
---
+++ pandora-client-web/src/components/chatroom/contextMenus/deviceContextMenu.tsx
@@ -0,0 +1,267 @@
+import { nanoid } from 'nanoid';
+import { ItemRoomDevice, AppearanceAction, ItemId } from 'pandora-common';
+import React, { useMemo, useCallback, useState, ReactElement, useEffect } from 'react';
+import { AppearanceContainer } from '../../../character/character';
+import { ChildrenProps } from '../../../common/reactTypes';
+import { PointLike } from '../../../graphics/graphicsCharacter';
+import { useContextMenuPosition } from '../../contextMenu';
+import { useChatRoomCharacters, useChatroom, useChatroomRequired, useRoomState } from '../../gameContext/chatRoomContextProvider';
+import { usePlayer } from '../../gameContext/playerContextProvider';
+import { useStaggeredAppearanceActionResult, useWardrobeContext, WardrobeContextProvider } from '../../wardrobe/wardrobe';
+import { EvalItemPath } from 'pandora-common/dist/assets/appearanceHelpers';
+
+function StoreDeviceMenu({ device, close }: {
+	device: ItemRoomDevice;
+	close: () => void;
+}) {
+	const { execute } = useWardrobeContext();
+	const action = useMemo<AppearanceAction>(() => ({
+		type: 'roomDeviceDeploy',
+		item: {
+			container: [],
+			itemId: device.id,
+		},
+		target: { type: 'roomInventory' },
+		deployment: null,
+	}), [device]);
+	const available = useStaggeredAppearanceActionResult(action, { immediate: true })?.result === 'success';
+	const onClick = useCallback(() => {
+		execute(action);
+		close();
+	}, [action, execute, close]);
+
+	if (!available) {
+		return null;
+	}
+
+	return (
+		<button onClick={ onClick }>
+			Store the device
+		</button>
+	);
+}
+
+function DeviceSlotClear({ device, slot, children, close }: ChildrenProps & {
+	device: ItemRoomDevice;
+	slot: string;
+	close: () => void;
+}) {
+	const { execute } = useWardrobeContext();
+	const action = useMemo<AppearanceAction>(() => ({
+		type: 'roomDeviceLeave',
+		item: {
+			container: [],
+			itemId: device.id,
+		},
+		target: { type: 'roomInventory' },
+		slot,
+	}), [device, slot]);
+	const available = useStaggeredAppearanceActionResult(action, { immediate: true })?.result === 'success';
+	const onClick = useCallback(() => {
+		if (action)
+			execute(action);
+
+		close();
+	}, [action, execute, close]);
+
+	if (!available) {
+		return null;
+	}
+
+	return (
+		<button onClick={ onClick }>
+			{ children }
+		</button>
+	);
+}
+
+function LeaveDeviceMenu({ device, close }: {
+	device: ItemRoomDevice;
+	close: () => void;
+}) {
+	const { player } = useWardrobeContext();
+	const slot = useMemo(() => [...device.slotOccupancy.entries()].find(([, id]) => id === player.id)?.[0], [device, player]);
+	if (!slot)
+		return null;
+
+	return (
+		<DeviceSlotClear device={ device } slot={ slot } close={ close }>
+			Exit the device
+		</DeviceSlotClear>
+	);
+}
+
+function OccupyDeviceSlotMenu({ device, slot, character, close }: {
+	device: ItemRoomDevice;
+	slot: string;
+	character: AppearanceContainer;
+	close: () => void;
+}) {
+	const { execute } = useWardrobeContext();
+	const action = useMemo<AppearanceAction>(() => ({
+		type: 'roomDeviceEnter',
+		item: {
+			container: [],
+			itemId: device.id,
+		},
+		target: { type: 'roomInventory' },
+		slot,
+		character: {
+			type: 'character',
+			characterId: character.id,
+		},
+		itemId: `i/${nanoid()}` as const,
+	}), [device, slot, character]);
+	const available = useStaggeredAppearanceActionResult(action, { immediate: true })?.result === 'success';
+	const onClick = useCallback(() => {
+		execute(action);
+		close();
+	}, [action, execute, close]);
+
+	if (!available) {
+		return null;
+	}
+
+	return (
+		<button onClick={ onClick }>
+			{ character.name } ({ character.id })
+		</button>
+	);
+}
+
+function DeviceSlotsMenu({ device }: {
+	device: ItemRoomDevice;
+	close: () => void;
+}) {
+	const [slot, setSlot] = useState<string | null>(null);
+	const occupancy = useMemo(() => slot && device.slotOccupancy.get(slot), [device, slot]);
+	const { player } = useWardrobeContext();
+	const chatRoomCharacters = useChatRoomCharacters();
+	const characters = useMemo<readonly AppearanceContainer[]>(() => chatRoomCharacters || [player], [chatRoomCharacters, player]);
+	const character = useMemo(() => characters.find(({ id }) => id === occupancy), [characters, occupancy]);
+
+	if (!slot) {
+		return (
+			<>
+				{ Object.entries(device.asset.definition.slots).map(([name, definition]) => (
+					<button key={ name } onClick={ () => setSlot(name) }>
+						{ definition.name }
+					</button>
+				)) }
+			</>
+		);
+	}
+
+	if (occupancy) {
+		return (
+			<>
+				<span>
+					{ device.asset.definition.slots[slot].name }
+				</span>
+				<span>
+					{ character?.name } ({ character?.id })
+				</span>
+				<DeviceSlotClear device={ device } slot={ slot } close={ close }>
+					{ (character)
+						? 'Exit the device'
+						: 'Clear occupancy of the slot' }
+				</DeviceSlotClear>
+				<button onClick={ () => setSlot(null) }>
+					Back to slots
+				</button>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<span>
+				{ device.asset.definition.slots[slot].name }
+			</span>
+			<span>
+				Enter:
+			</span>
+			{ characters.map((char) => (
+				<OccupyDeviceSlotMenu key={ char.id } device={ device } slot={ slot } character={ char } close={ close } />
+			)) }
+			<button onClick={ () => setSlot(null) }>
+				Back to slots
+			</button>
+		</>
+	);
+}
+
+function DeviceContextMenuCurrent({ device, position, onClose }: {
+	device: ItemRoomDevice;
+	position: Readonly<PointLike>;
+	onClose: () => void;
+}): ReactElement | null {
+	const ref = useContextMenuPosition(position);
+	const player = usePlayer();
+	const chatRoom = useChatroom();
+	const [menu, setMenu] = useState<'main' | 'slots'>('main');
+
+	if (!player || !chatRoom) {
+		return null;
+	}
+
+	return (
+		<div className='context-menu' ref={ ref } onPointerDown={ (e) => e.stopPropagation() }>
+			<span>
+				{ device.asset.definition.name }
+			</span>
+			<WardrobeContextProvider target={ chatRoom } player={ player }>
+				{ menu === 'main' && (
+					<>
+						<LeaveDeviceMenu device={ device } close={ onClose } />
+						<button onClick={ () => setMenu('slots') }>
+							Slots
+						</button>
+						<StoreDeviceMenu device={ device } close={ onClose } />
+					</>
+				) }
+				{ menu === 'slots' && (
+					<>
+						<DeviceSlotsMenu device={ device } close={ onClose } />
+						<button onClick={ () => setMenu('main') }>
+							Back
+						</button>
+					</>
+				) }
+			</WardrobeContextProvider>
+			<button onClick={ onClose } >
+				Close
+			</button>
+		</div>
+	);
+}
+
+export function DeviceContextMenu({ deviceItemId, position, onClose }: {
+	deviceItemId: ItemId;
+	position: Readonly<PointLike>;
+	onClose: () => void;
+}): ReactElement | null {
+	const globalState = useRoomState(useChatroomRequired());
+	const item = useMemo(() => {
+		const actual = globalState.getItems({ type: 'roomInventory' });
+		if (!actual)
+			return null;
+
+		return EvalItemPath(actual, {
+			container: [],
+			itemId: deviceItemId,
+		});
+	}, [globalState, deviceItemId]);
+
+	useEffect(() => {
+		if (!item?.isType('roomDevice'))
+			onClose();
+	}, [item, onClose]);
+
+	if (!item?.isType('roomDevice'))
+		return null;
+
+	return (
+		<DeviceContextMenuCurrent device={ item } position={ position } onClose={ onClose } />
+	);
+}

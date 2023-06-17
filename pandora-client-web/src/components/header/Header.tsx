d478f2179aacc872282b9fb327b60e71860e9dc0
Sekkmer <sekkmer@gmail.com>
2023-06-17 20:17:04 +0200

    [CHANGE] unify leave actions to the top right button (#297)
    Co-authored-by: Jomshir98 <jomshir98@protonmail.com>

--- pandora-client-web/src/components/header/Header.tsx
+++ pandora-client-web/src/components/header/Header.tsx
@@ -1,15 +1,13 @@
 import classNames from 'classnames';
-import { EMPTY, IsAuthorized } from 'pandora-common';
+import { IsAuthorized } from 'pandora-common';
 import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import friendsIcon from '../../assets/icons/friends.svg';
-import logoutIcon from '../../assets/icons/logout.svg';
 import notificationsIcon from '../../assets/icons/notification.svg';
 import settingsIcon from '../../assets/icons/setting.svg';
 import wikiIcon from '../../assets/icons/wiki.svg';
 import managementIcon from '../../assets/icons/management.svg';
 import { usePlayerData, usePlayerState } from '../gameContext/playerContextProvider';
-import { useLogout } from '../../networking/account_manager';
 import { useCurrentAccount, useDirectoryConnector } from '../gameContext/directoryConnectorContextProvider';
 import { useShardConnectionInfo } from '../gameContext/shardConnectorContextProvider';
 import './header.scss';
@@ -22,6 +20,7 @@ import { useCharacterSafemode } from '../../character/character';
 import { useSafemodeDialogContext } from '../characterSafemode/characterSafemode';
 import { RelationshipContext, useRelationships } from '../releationships/relationships';
 import { useObservable } from '../../observable';
+import { LeaveButton } from './leaveButton';
 
 function LeftHeader(): ReactElement {
 	const connectionInfo = useShardConnectionInfo();
@@ -53,7 +52,6 @@ function LeftHeader(): ReactElement {
 }
 
 function CharacterMenu({ close }: { close: () => void; }): ReactElement {
-	const directoryConnector = useDirectoryConnector();
 	const playerState = usePlayerState();
 
 	const safemode = useCharacterSafemode(playerState);
@@ -62,12 +60,6 @@ function CharacterMenu({ close }: { close: () => void; }): ReactElement {
 	return (
 		<div className='characterMenu' onClick={ () => close() }>
 			<header onClick={ (ev) => ev.stopPropagation() }>Character menu</header>
-			<a onClick={ (ev) => {
-				ev.preventDefault();
-				directoryConnector.sendMessage('disconnectCharacter', EMPTY);
-			} }>
-				Change character
-			</a>
 			<a onClick={ (ev) => {
 				ev.preventDefault();
 				safemodeContext.show();
@@ -80,7 +72,6 @@ function CharacterMenu({ close }: { close: () => void; }): ReactElement {
 
 function RightHeader(): ReactElement {
 	const currentAccount = useCurrentAccount();
-	const logout = useLogout();
 	const navigate = useNavigate();
 	const loggedIn = currentAccount != null;
 
@@ -96,7 +87,7 @@ function RightHeader(): ReactElement {
 					<HeaderButton icon={ settingsIcon } iconAlt='Settings' onClick={ () => navigate('/settings') } title='Settings' />
 					{ isDeveloper && <HeaderButton icon={ managementIcon } iconAlt='Settings' onClick={ () => navigate('/management') } title='Management' /> }
 					<span>{ currentAccount.username }</span>
-					<HeaderButton icon={ logoutIcon } iconAlt='Logout' onClick={ logout } title='Logout' />
+					<LeaveButton />
 				</>
 			) }
 			{ !loggedIn && <span>[not logged in]</span> }
---
+++ pandora-client-web/src/components/header/leaveButton.tsx
@@ -0,0 +1,166 @@
+import React, { ReactElement, createContext, useCallback, useContext, useState } from 'react';
+import logoutIcon from '../../assets/icons/logout.svg';
+import { HeaderButton } from './HeaderButton';
+import { useShardConnectionInfo } from '../gameContext/shardConnectorContextProvider';
+import { usePlayer, usePlayerData, usePlayerState } from '../gameContext/playerContextProvider';
+import { useCurrentAccount, useDirectoryConnector } from '../gameContext/directoryConnectorContextProvider';
+import { EMPTY, GetLogger, IChatRoomFullInfo } from 'pandora-common';
+import { Button } from '../common/button/button';
+import { useLogout } from '../../networking/account_manager';
+import { useCharacterRestrictionsManager, useChatRoomInfo } from '../gameContext/chatRoomContextProvider';
+import { PlayerCharacter } from '../../character/player';
+import { toast } from 'react-toastify';
+import { TOAST_OPTIONS_ERROR } from '../../persistentToast';
+import { ModalDialog } from '../dialog/dialog';
+import { Column, Row } from '../common/container/container';
+import './leaveButton.scss';
+
+const leaveButtonContext = createContext(() => { /** noop */ });
+
+export function LeaveButton() {
+	const [show, setShow] = useState(false);
+	const closeDialog = useCallback(() => setShow(false), []);
+
+	return (
+		<leaveButtonContext.Provider value={ closeDialog }>
+			<HeaderButton icon={ logoutIcon } iconAlt='Leave' onClick={ () => setShow(true) } title='Leave' />
+			{
+				show ? <DialogLeave /> : null
+			}
+		</leaveButtonContext.Provider>
+	);
+}
+
+function DialogLeave(): ReactElement {
+	const closeDialog = useContext(leaveButtonContext);
+
+	return (
+		<ModalDialog>
+			<Column className='LeaveDialog'>
+				<ChatRoomLeave />
+				<CharacterLeave />
+				<AccountLeave />
+				<Button onClick={ closeDialog }>Cancel</Button>
+			</Column>
+		</ModalDialog>
+	);
+}
+
+function ChatRoomLeave(): ReactElement {
+	const player = usePlayer();
+	const room = useChatRoomInfo();
+
+	return (
+		<fieldset>
+			<legend>Chat Room</legend>
+			{
+				(player && room) ? (
+					<CharRoomLeaveInner player={ player } room={ room } />
+				) : (
+					<span>Not in a chat room</span>
+				)
+			}
+		</fieldset>
+	);
+}
+
+function CharRoomLeaveInner({ player, room }: { player: PlayerCharacter; room: IChatRoomFullInfo; }): ReactElement {
+	const directoryConnector = useDirectoryConnector();
+	const playerState = usePlayerState();
+	const canLeave = useCharacterRestrictionsManager(playerState, player, (manager) => (manager.isInSafemode() || !manager.getEffects().blockRoomLeave));
+	const closeDialog = useContext(leaveButtonContext);
+
+	const onRoomLeave = useCallback(() => {
+		directoryConnector.awaitResponse('chatRoomLeave', EMPTY)
+			.then((result) => {
+				if (result.result !== 'ok') {
+					toast(`Failed to leave room:\n${result.result}`, TOAST_OPTIONS_ERROR);
+				} else {
+					closeDialog();
+				}
+			})
+			.catch((err) => {
+				GetLogger('LeaveRoom').warning('Error while leaving room', err);
+				toast(`Error while leaving room:\n${err instanceof Error ? err.message : String(err)}`, TOAST_OPTIONS_ERROR);
+			});
+	}, [directoryConnector, closeDialog]);
+
+	return (
+		<>
+			<span>Name: { room.name }</span>
+			<span>Id: { room.id }</span>
+			{
+				!canLeave ? (
+					<Row alignX='center' padding='large'>
+						<b>An item is preventing you from leaving the room.</b>
+					</Row>
+				) : null
+			}
+			<Button onClick={ onRoomLeave } className='fadeDisabled' disabled={ !canLeave }>
+				Leave room
+			</Button>
+		</>
+	);
+}
+
+function CharacterLeave(): ReactElement {
+	const directoryConnector = useDirectoryConnector();
+	const connectionInfo = useShardConnectionInfo();
+	const characterData = usePlayerData();
+	const characterName = (characterData && !characterData.inCreation) ? characterData.name : null;
+	const closeDialog = useContext(leaveButtonContext);
+
+	const onClick = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		directoryConnector.sendMessage('disconnectCharacter', EMPTY);
+		closeDialog();
+	}, [directoryConnector, closeDialog]);
+
+	return (
+		<fieldset>
+			<legend>Character</legend>
+			{
+				connectionInfo ? (
+					<>
+						<span>Name: { characterName ?? `[Character ${connectionInfo.characterId}]` }</span>
+						<span>Id: { connectionInfo.characterId }</span>
+						<Button onClick={ onClick }>Change character</Button>
+					</>
+				) : (
+					<span>No character selected</span>
+				)
+			}
+		</fieldset>
+	);
+}
+
+function AccountLeave(): ReactElement {
+	const currentAccount = useCurrentAccount();
+	const logout = useLogout();
+	const closeDialog = useContext(leaveButtonContext);
+
+	const onClick = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		logout();
+		closeDialog();
+	}, [logout, closeDialog]);
+
+	return (
+		<fieldset>
+			<legend>Account</legend>
+			{
+				currentAccount ? (
+					<>
+						<span>Name: { currentAccount.username }</span>
+						<span>Id: { currentAccount.id }</span>
+						<Button onClick={ onClick }>Logout</Button>
+					</>
+				) : (
+					<span>Not logged in</span>
+				)
+			}
+		</fieldset>
+	);
+}

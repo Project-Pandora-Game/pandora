ce05f163060a7145a7dbee9d1fc4f8c98277ce46
jomshir98 <jomshir98@protonmail.com>
2023-09-26 10:34:02 +0000

    [ADD] Generic selection indicator component

---
+++ pandora-client-web/src/components/common/selectionIndicator/selectionIndicator.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from 'react';
+import './selectionIndicator.scss';
+import { DivContainer, DivContainerProps } from '../container/container';
+import classNames from 'classnames';
+
+export interface SelectionIndicatorProps extends DivContainerProps {
+	/** @default false */
+	selected?: boolean;
+	/** @default false */
+	active?: boolean;
+}
+
+export function SelectionIndicator({
+	selected = false,
+	active = false,
+	children,
+	className,
+	...divContainerProps
+}: SelectionIndicatorProps): ReactElement {
+	return (
+		<DivContainer
+			padding='tiny'
+			{ ...divContainerProps }
+			className={ classNames(
+				'selectionIndicator',
+				selected ? 'selected' : null,
+				active ? 'active' : null,
+				className,
+			) }
+		>
+			{ children }
+		</DivContainer>
+	);
+}

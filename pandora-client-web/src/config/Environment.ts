50cad5abeb3d8cd9d218de40a67da08e02f11dc8
Sekkmer <sekkmer@gmail.com>
2023-10-04 20:08:33 +0200

    [REFACTOR] use new env parser for client

--- pandora-client-web/src/config/Environment.ts
+++ pandora-client-web/src/config/Environment.ts
@@ -1,15 +1,16 @@
+import { CreateEnvParser } from 'pandora-common';
+import { CLIENT_CONFIG } from './definition';
+
 export type NodeEnv = 'production' | 'development';
 
-// Provided by Webpack
-declare const process: {
-	env: Record<string, unknown>;
-};
+export const EnvParser = CreateEnvParser(CLIENT_CONFIG);
+export const ENV = EnvParser();
 
-export const GAME_NAME = process.env.GAME_NAME as string;
-export const GAME_VERSION = process.env.VERSION as string;
-export const NODE_ENV = process.env.NODE_ENV as NodeEnv;
-export const DIRECTORY_ADDRESS = process.env.DIRECTORY_ADDRESS as string;
-export const EDITOR_ASSETS_ADDRESS = process.env.EDITOR_ASSETS_ADDRESS as string;
-export const USER_DEBUG = NODE_ENV !== 'production' || process.env.USER_DEBUG === 'true';
-export const GIT_COMMIT_HASH = process.env.GIT_COMMIT_HASH as string;
-export const GIT_DESCRIBE = process.env.GIT_DESCRIBE as string;
+export const GAME_NAME = ENV.GAME_NAME;
+export const GAME_VERSION = ENV.GAME_VERSION;
+export const NODE_ENV = ENV.NODE_ENV;
+export const DIRECTORY_ADDRESS = ENV.DIRECTORY_ADDRESS;
+export const EDITOR_ASSETS_ADDRESS = ENV.EDITOR_ASSETS_ADDRESS;
+export const USER_DEBUG = NODE_ENV !== 'production' || ENV.USER_DEBUG;
+export const GIT_COMMIT_HASH = ENV.GIT_COMMIT_HASH;
+export const GIT_DESCRIBE = ENV.GIT_DESCRIBE;
---
+++ pandora-client-web/src/config/definition.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const WEBPACK_BASE_CONFIG = {
+	DIRECTORY_ADDRESS: z.string().default('http://127.0.0.1:25560'),
+	EDITOR_ASSETS_ADDRESS: z.string().default('http://127.0.0.1:26969/assets'),
+	USER_DEBUG: z.boolean().default(false),
+} as const;
+
+export const CLIENT_CONFIG = {
+	...WEBPACK_BASE_CONFIG,
+	GAME_NAME: z.string(),
+	GAME_VERSION: z.string(),
+	NODE_ENV: z.enum(['production', 'development']),
+	GIT_COMMIT_HASH: z.string(),
+	GIT_DESCRIBE: z.string(),
+} as const;

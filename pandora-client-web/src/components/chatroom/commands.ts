72130f217884d4ebb454ebb5fdeca245c937d574
jomshir98 <jomshir98@protonmail.com>
2023-10-15 12:04:19 +0000

    [CHANGE] Improve url-based tab container

--- pandora-client-web/src/components/chatroom/commands.ts
+++ pandora-client-web/src/components/chatroom/commands.ts
@@ -200,7 +200,7 @@ export const COMMANDS: readonly IClientCommand[] = [
 			.argument('target', CommandSelectorCharacter({ allowSelf: 'none' }))
 			.handler(({ directoryConnector, navigate }, { target }) => {
 				directoryConnector.directMessageHandler.setSelected(target.data.accountId);
-				navigate('/relationships/DMs');
+				navigate('/relationships/dm');
 				return true;
 			}),
 	},

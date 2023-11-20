fc71982d3458c3c6702ab4f328d2bee22b9e16ec
jomshir98 <jomshir98@protonmail.com>
2023-11-20 21:28:03 +0000

    [ADD] Add message to EULA that all characters are adults
    Co-authored-by: ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>

--- pandora-client-web/src/components/Eula/index.tsx
+++ pandora-client-web/src/components/Eula/index.tsx
@@ -28,7 +28,8 @@ export function Eula({ accept }: EulaProps): ReactElement {
 				</div>
 				<div className='eula-text'>
 					<p>
-						Welcome to { GAME_NAME }! This game is intended for use by adults only.
+						Welcome to { GAME_NAME }! This game is intended for use by adults only.<br />
+						All characters portrayed in this game are fictitious and of legal age. No identification with actual persons is intended or should be inferred.
 					</p>
 					<p>
 						By playing this game, you agree to the following:

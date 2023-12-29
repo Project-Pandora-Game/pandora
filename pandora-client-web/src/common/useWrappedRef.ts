6d814b0319de89eb44d5b7f80e18222c5ce3de6c
Sekkmer <sekkmer@gmail.com>
2023-12-29 20:27:31 +0100

    [FIX] better type inside useWrappedRef

--- pandora-client-web/src/common/useWrappedRef.ts
+++ pandora-client-web/src/common/useWrappedRef.ts
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function useWrappedRef<Element extends HTMLElement>(originalRef: React.ForwardedRef<Element>): React.RefObject<Element> {
 	return React.useMemo(() => {
-		let current = null as Element | null;
+		let current: Element | null = null;
 		return {
-			get current() {
+			get current(): Element | null {
 				return current;
 			},
 			set current(value: Element | null) {

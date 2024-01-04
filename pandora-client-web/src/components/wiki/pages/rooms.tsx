fb1ef8c6e6d3528da37bb95ff92011db4c1539fb
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-01-04 19:49:02 +0100

    [ADD] finished draft of the rooms tab

--- pandora-client-web/src/components/wiki/pages/rooms.tsx
+++ pandora-client-web/src/components/wiki/pages/rooms.tsx
@@ -8,33 +8,19 @@ export function WikiRooms(): ReactElement {
 			<h3>Introduction</h3>
 
 			<p>
-				REVISE THIS TEXT!
-				In Pandora, each room is persistent (settings, inventory, and room devices stay even after everyone has left) and has one or more owners.
-				It only gets deleted when it no longer has any owners.<br />
-				A room is visible to everyone (except accounts banned from the room), if it is marked as public and there is at least one admin inside the room.
-				You can always see rooms you are either admin or owner of.<br />
-				When a character goes offline inside a room, they will stay in the room and are shown as not connected.
-				By default, this is shown by the character having a ghost-like effect; this effect can be changed in the settings.<br />
-				Rooms can have a long description that can be read from the outside in the room preview popup.
+				A room in Pandora always exists in the state it was set up, even when it was empty for a long time.
+				You can decorate a room with room items/furniture and it can theoretically be joined by up to 100 characters at the same time.<br />
+				The "Chat"-tab next to the room view can also be switched to:
 			</p>
-			<p>
-				Each <strong>account</strong> has a maximum number of rooms it can own. You can view the rooms owned by your account
-				as well as your ownership limit on the room search screen.
-				If you want to create another room beyond your room ownership limit, you must select any of your owned rooms and either repurpose it or
-				give up ownership of that room (resulting in the room being deleted if it has no other owners).
-			</p>
-			<p>
-				Every character has their own <strong>personal room</strong> that does not count towards this limit and that cannot be entered by any
-				other character. It functions as a singleplayer lobby and cannot be deleted or given up.<br />
-				You will automatically end up in the personal room when your selected character is not in any multiplayer room. Restraining effects will not
-				prevent you from leaving the personal room.
-			</p>
+			<ul>
+				<li>the "Room"-tab that lets you configure the room and shows a list of all characters currently inside the room</li>
+				<li>the "Pose"-tab that lets you change your character's pose</li>
+				<li>the "Expressions"-tab that lets you change your character's facial expressions and body states</li>
+			</ul>
 
 			<h3>Room-specific features</h3>
 			<ul>
-				{
-					// <li><a href='#RO_'></a></li>
-				}
+				<li><a href='#RO_Room_layout'>Room layout</a></li>
 				<li><a href='#RO_Room_ownership'>Room ownership</a></li>
 				<li><a href='#RO_Room_deletion'>Room deletion</a></li>
 				<li><a href='#RO_Room_persistence'>Room persistence</a></li>
@@ -44,7 +30,24 @@ export function WikiRooms(): ReactElement {
 				<li><a href='#RO_Leaving_a_room'>Leaving a room</a></li>
 				<li><a href='#RO_Personal_room'>Personal room</a></li>
 				<li><a href='#RO_Room_inventory'>Room inventory</a></li>
-				<li><a href='#RO_Room_chat_Text_formatting'>Room Chat: Text formatting</a></li>
+				<li><a href='#RO_Room_chat_Chat_commands'>Room chat: Chat commands</a></li>
+				<li><a href='#RO_Room_chat_Chat_modes'>Room chat: Chat modes</a></li>
+				<li><a href='#RO_Room_chat_Editing_text'>Room chat: Editing text</a></li>
+				<li><a href='#RO_Room_chat_Whispering_someone'>Room chat: Whispering someone</a></li>
+				<li><a href='#RO_Room_chat_Text_formatting'>Room chat: Text formatting</a></li>
+			</ul>
+
+			<h4 id='RO_Room_layout'>Room layout</h4>
+			<p>
+				The room view in Pandora consists of the room canvas and the four tabs that show the chat per default.<br />
+				In a landscape-ratio window the tab is on the right side and it is on the bottom in a portrait-ratio window.
+				In the Pandora settings (cog-button on the top bar) under the "Interface"-tab and inside the "Chatroom UI" box,
+				you can customize the ratio between the space that the room and the tabs area use for both landscape or portrait views.
+			</p>
+			<ul>
+				<li>You can zoom the room canvas with the mouse wheel or a pinch-to-zoom gesture.</li>
+				<li>You can drag the canvas freely to see a different part of it while zoomed in.</li>
+				<li>If you experience performance issues, you can lower/disable graphics related features in Pandora's settings.</li>
 			</ul>
 
 			<h4 id='RO_Room_ownership'>Room ownership</h4>
@@ -136,17 +139,72 @@ export function WikiRooms(): ReactElement {
 
 			<h4 id='RO_Room_inventory'>Room inventory</h4>
 			<p>
-				TODO
+				The room inventory shows all items that are inside the current room and can be picked up or used by other characters, if permitted.<br />
+				From this screen, you can also create new items inside the room inventory, edit them, or delete them.
+				Clicking on an item in the list opens its edit-view on the right side.
+				<a href='/wiki/items/#IT_Room-level_items'>Room devices</a> can be deployed to the room background in the edit-view by permitted parties.
 			</p>
+
+			<h4 id='RO_Room_chat_Chat_commands'>Room chat: Chat commands</h4>
+			<p>
+				There are also chat commands that either act as shortcuts to certain features or offer new functionality, such as playing games in the chat.
+				Type "/" into the chat field to see a list of all chat commands.
+			</p>
+			<ul>
+				<li>The "tab"-key will try to autocomplete commands you started typing or cycle through the possible matches shown</li>
+				<li>The "tab"-key can also be used to autocomplete command arguments such as target characters or cycle through all of them</li>
+			</ul>
+
+			<h4 id='RO_Room_chat_Chat_modes'>Room chat: Chat modes</h4>
+			<p>
+				The chat is always in a specific chat mode, such as emote mode or OOC mode.
+				The default mode is writing normal text to the whole room that can be formatted.<br />
+				You can write in another chat mode by either using a chat command or by pressing the bar with the white cog above the input field.
+				This toggles a dropdown menu that lets you switch to a different chat mode.<br />
+			</p>
+			<ul>
+				<li>Chat modes are persisting over several messages.</li>
+				<li>You can either switch to another chat mode or cancel it, which implicitly switches to the default chat mode</li>
+				<li>If you use a chat command for sending a message in a certain chat mode (e.g. "/me") you will stay in your previous chat mode.</li>
+				<li>If you want to permanently switch to another chat mode via a chat command, send the command without a text message behind the command.</li>
+				<li>Certain chat modes can be combined with the whisper mode.</li>
+				<li>All chat modes have a variant to send text without any formatting, which are the "raw" variants of the according chat commands.</li>
+			</ul>
+
+			<h4 id='RO_Room_chat_Editing_text'>Room chat: Editing text</h4>
+			<p>
+				Pandora allows you to edit or even delete text you sent in the chat during the first minutes after posting it.<br />
+				This works by right-clicking on any past text of yours or using the "Arrow-up" key to edit the last message specifically.
+			</p>
+			<ul>
+				<li>You can edit and delete whispers, emotes and general chat messages like that.</li>
+				<li>After several minutes have passed, the menu no longer shows the option to edit or delete the text.</li>
+				<li>The message is tagged with a small "[edited]" on the right, next to the updated time stamp.</li>
+			</ul>
+
+			<h4 id='RO_Room_chat_Whispering_someone'>Room chat: Whispering someone</h4>
+			<p>
+				There are several ways to whisper someone in the same chat room.<br />
+				Any of them puts you into a whisper mode that you need to explicitly leave by using the "Cancel"-button above the chat input.
+			</p>
+			<ul>
+				<li>You can click on a character name in the chat itself.</li>
+				<li>You can click on a character name under a character in the room and select "Whisper".</li>
+				<li>You can click on the "Whisper"-button next to a character name in the "Room"-tab.</li>
+				<li>You can use the chat command "/w [target]" using either the character name or the character ID as whisper target argument.</li>
+				<li>Chat-related commands while in whisper mode (e.g., "/me") will be executed normally and not be whispered.</li>
+				<li>If your whisper target leaves the room, your whisper message cannot be sent.</li>
+				<li>If your whisper target goes offline, your whisper message will still be sent, but currently it will not be delivered.</li>
+			</ul>
 
 			<h4 id='RO_Room_chat_Text_formatting'>Room chat: Text formatting</h4>
 			<p>
-				Pandora supports markdown syntax to write text italic or bold: "__bold text__" | "_italicized  text_"<br />
+				Pandora supports a part of the markdown syntax to write text italic or bold: "__bold text__" or "_italicized  text_"<br />
 				While the chat room input field is focused, you can also use keyboard shortcuts to get the supported markdown syntax:
 			</p>
 			<ul>
-				<li>Use "ctrl+b" to style the next new characters in bold or mark text and then use this shortcut.</li>
-				<li>Use "ctrl+i" to style the next new characters in italic or mark text and then use this shortcut.</li>
+				<li>Use "ctrl+b" to style the next new characters in bold or mark text and then use the shortcut.</li>
+				<li>Use "ctrl+i" to style the next new characters in italic or mark text and then use the shortcut.</li>
 				<li>No other markdown syntax is currently supported</li>
 			</ul>
 

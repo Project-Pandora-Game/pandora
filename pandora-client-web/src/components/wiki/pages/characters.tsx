f94fc4831cf32ee8997aa4d59af33f34b97a90f2
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-02-19 11:51:07 +0100

    [ADD] wiki section on character immersion effects

--- pandora-client-web/src/components/wiki/pages/characters.tsx
+++ pandora-client-web/src/components/wiki/pages/characters.tsx
@@ -21,11 +21,48 @@ export function WikiCharacters(): ReactElement {
 
 			<h3>Character-specific features</h3>
 			<ul>
+				<li><a href='#CH_Character_immersion'>Character immersion effects</a></li>
 				<li><a href='#CH_Character_movement'>Character movement</a></li>
 				<li><a href='#CH_Character_context_menu'>Character context menu</a></li>
 				<li><a href='#CH_Character_wardrobe'>Character wardrobe</a></li>
 				<li><a href='#CH_Character_permissions'>Character permissions</a></li>
 			</ul>
+
+			<h4 id='CH_Character_immersion'>Character immersion effects</h4>
+			<p>
+				Items in Pandora can put certain immersion enhancing effects on a character. Restraints...
+			</p>
+			<ul>
+				<li>can prevent you from interacting with items, by blocking your hand usage.</li>
+				<li>can prevent you from moving around and/or leaving the current room.</li>
+				<li>with a seeing impairment effect will darken your view of the room partially or completely.</li>
+				<li>with a speech impairment effect will muffle your normal and whisper messages so that others have a hard time making sense out of them.</li>
+				<li>with a hearing impairment will muffle the messages you receive so that you have a hard time making sense out of them.</li>
+			</ul>
+			<p>
+				Items with such effects cannot just be used as a cosmetic to make your character look nice. They come with consistent experiences.
+				Predictability is important. Every user should be able to trust that items with these effects are working as expected
+				when they are being used on someone.
+				If you dislike any of these effects, such as seeing a fully black room view with certain heavy blindfolds, you
+				can simply <a href='/wiki/items/#IT_Item_preferences_and_limits'>block according items.</a>
+				Note that items come with different strength levels of such effects, so it is feasible for a user to to slowly learn to understand lighter
+				levels of "gag-talk" to a certain degree, by making sense out of the garbled message.
+			</p>
+			<p>
+				Note on roleplaying with communication impairments:<br />
+				While in a scene, someone may be of the view that not being able to understand speech is a problem in terms of making
+				decisions on how to follow-up with the current play. They may want a way to understand "gag-talk" to feel more safe and confident.
+				However, it is simply not realistic to expect such information when facing the effects of such restraints and breaks immersion.
+				All users know that in Pandora muffled speech cannot be assumed as understandable. It is a consistent experience for all users.
+				Choose do not use gags, if you want someone to be able to speak clearly, or use gags with a light effect.<br />
+				Moreover, when the play partner supports the scene well, it is also not necessary to understand "gag-talk" to feel on top of things.
+				Users with gag effects on their character should
+				describe and convey important non-verbal communication towards the play partner in the
+				form of <a href='/wiki/items/#SP_Room_chat_Chat_modes'>emotes</a>.
+				This can be describing things the play partner could notice, such as discomfort, the state of the own body, or a hint towards
+				what the muffled sentence likely meant from the tone and strength of the voice. A good roleplaying scene relies on all parties
+				to describe the scene in detail!
+			</p>
 
 			<h4 id='CH_Character_movement'>Character movement</h4>
 			<p>
@@ -35,7 +72,7 @@ export function WikiCharacters(): ReactElement {
 			</p>
 			<ul>
 				<li>If you have problems dragging the name because it is too small, you can zoom in with mouse wheel or pinch-to-zoom gesture.</li>
-				<li>Items can prevent or slow down character movement.</li>
+				<li>Items can prevent character movement.</li>
 				<li>
 					The "Character Y Offset" value inside the "Pose"-tab can shift your character upwards and downwards alongside the z-axis
 					without changing your character's relative size in an unrealistic way.

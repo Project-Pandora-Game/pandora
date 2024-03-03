7e23f44dca3c816b8df5fd40f3cf5305482c23a3
Sekkmer <sekkmer@gmail.com>
2024-03-03 15:02:38 +0100

    [REFACTOR] move collection specific code into new class
    move migration and index updating

--- pandora-server-directory/src/database/mongoDb.ts
+++ pandora-server-directory/src/database/mongoDb.ts
@@ -1,15 +1,15 @@
 import AsyncLock from 'async-lock';
-import { diffString } from 'json-diff';
-import _, { isEqual, omit } from 'lodash';
-import { CollationOptions, Collection, Db, IndexDescription, MatchKeysAndValues, MongoClient, ObjectId } from 'mongodb';
+import _ from 'lodash';
+import { CollationOptions, Db, MatchKeysAndValues, MongoClient } from 'mongodb';
 import type { MongoMemoryServer } from 'mongodb-memory-server-core';
 import { nanoid } from 'nanoid';
-import { AccountId, ArrayToRecordKeys, Assert, AssertNotNullable, CharacterDataSchema, CharacterId, GetLogger, ICharacterData, ICharacterDataDirectoryUpdate, ICharacterDataShardUpdate, ICharacterSelfInfoUpdate, IDirectoryDirectMessage, IsObject, KnownObject, SPACE_DIRECTORY_PROPERTIES, SpaceData, SpaceDataDirectoryUpdate, SpaceDataSchema, SpaceDataShardUpdate, SpaceDirectoryData, SpaceId, ZodCast } from 'pandora-common';
-import { ZodType, ZodTypeDef, z } from 'zod';
+import { AccountId, ArrayToRecordKeys, Assert, AssertNotNullable, CharacterDataSchema, CharacterId, GetLogger, ICharacterData, ICharacterDataDirectoryUpdate, ICharacterDataShardUpdate, ICharacterSelfInfoUpdate, IDirectoryDirectMessage, SPACE_DIRECTORY_PROPERTIES, SpaceData, SpaceDataDirectoryUpdate, SpaceDataSchema, SpaceDataShardUpdate, SpaceDirectoryData, SpaceId, ZodCast } from 'pandora-common';
+import { z } from 'zod';
 import { ENV } from '../config';
 import type { ICharacterSelfInfoDb, PandoraDatabase } from './databaseProvider';
 import { DATABASE_ACCOUNT_UPDATEABLE_PROPERTIES, DatabaseAccount, DatabaseAccountContact, DatabaseAccountContactType, DatabaseAccountSchema, DatabaseAccountSecure, DatabaseAccountUpdate, DatabaseAccountWithSecure, DatabaseAccountWithSecureSchema, DatabaseConfigData, DatabaseConfigType, DatabaseDirectMessageInfo, DirectMessageAccounts } from './databaseStructure';
 import { CreateCharacter, CreateSpace, SpaceCreationData } from './dbHelper';
+import { ValidatedCollection, DbAutomaticMigration, ValidatedCollectionType } from './validatedCollection';
 
 const { DATABASE_URL, DATABASE_NAME, DATABASE_MIGRATION } = ENV;
 const logger = GetLogger('db');
@@ -35,18 +35,114 @@ type MongoDbInit = Readonly<{
 
 export const MONGODB_SERVER_VERSION: string = '6.0.5';
 
+const accountCollection = new ValidatedCollection(
+	logger,
+	ACCOUNTS_COLLECTION_NAME,
+	DatabaseAccountWithSecureSchema,
+	[
+		{
+			name: 'id',
+			unique: true,
+			key: { id: 1 },
+		},
+		{
+			name: 'username',
+			unique: true,
+			key: { username: 1 },
+			// Usernames are case-insensitive
+			collation: COLLATION_CASE_INSENSITIVE,
+		},
+		{
+			name: 'emailHash',
+			unique: true,
+			key: { 'secure.emailHash': 1 },
+		},
+		{
+			name: 'githubId',
+			unique: true,
+			key: { 'secure.github.id': 1 },
+			// Ignore accounts without github data
+			sparse: true,
+		},
+	],
+);
+
+const characterCollection = new ValidatedCollection(
+	logger,
+	CHARACTERS_COLLECTION_NAME,
+	CharacterDataSchema.omit({ id: true }).and(z.object({ id: z.number().int() })),
+	[
+		{
+			name: 'id',
+			unique: true,
+			key: { id: 1 },
+		},
+	],
+);
+
+const accountContactCollection = new ValidatedCollection(
+	logger,
+	ACCOUNT_CONTACTS_COLLECTION_NAME,
+	ZodCast<DatabaseAccountContact>(), // TODO: This needs a proper schema!
+	[
+		{
+			name: 'accounts',
+			key: { accounts: 1 },
+		},
+	],
+);
+
+const spaceCollection = new ValidatedCollection(
+	logger,
+	SPACES_COLLECTION_NAME,
+	SpaceDataSchema,
+	[
+		{
+			name: 'id',
+			unique: true,
+			key: { id: 1 },
+		},
+	],
+);
+
+const configCollection = new ValidatedCollection(
+	logger,
+	'config',
+	ZodCast<{ type: DatabaseConfigType; data: DatabaseConfigData<DatabaseConfigType>; }>(), // TODO: This needs a proper schema!
+	[
+		{
+			name: 'id',
+			unique: true,
+			key: { type: 1 },
+		},
+	],
+);
+
+const directMessageCollection = new ValidatedCollection(
+	logger,
+	DIRECT_MESSAGES_COLLECTION_NAME,
+	ZodCast<IDirectoryDirectMessage & { accounts: DirectMessageAccounts; }>(), // TODO: This needs a proper schema!
+	[
+		{
+			name: 'accounts',
+			unique: true,
+			key: { accounts: 1 },
+		},
+	],
+);
+
 export default class MongoDatabase implements PandoraDatabase {
 	private readonly _lock: AsyncLock;
 	private readonly _init: MongoDbInit;
 	private _client!: MongoClient;
 	private _inMemoryServer!: MongoMemoryServer;
 	private _db!: Db;
-	private _accounts!: Collection<DatabaseAccountWithSecure>;
-	private _characters!: Collection<Omit<ICharacterData, 'id'> & { id: number; }>;
-	private _accountContacts!: Collection<DatabaseAccountContact>;
-	private _spaces!: Collection<SpaceData>;
-	private _config!: Collection<{ type: DatabaseConfigType; data: DatabaseConfigData<DatabaseConfigType>; }>;
-	private _directMessages!: Collection<IDirectoryDirectMessage & { accounts: DirectMessageAccounts; }>;
+	private _accounts!: ValidatedCollectionType<typeof accountCollection>;
+	private _characters!: ValidatedCollectionType<typeof characterCollection>;
+	private _accountContacts!: ValidatedCollectionType<typeof accountContactCollection>;
+	private _spaces!: ValidatedCollectionType<typeof spaceCollection>;
+	private _config!: ValidatedCollectionType<typeof configCollection>;
+	private _directMessages!: ValidatedCollectionType<typeof directMessageCollection>;
 	private _nextAccountId = 1;
 	private _nextCharacterId = 1;
 
@@ -78,107 +174,46 @@ export default class MongoDatabase implements PandoraDatabase {
 
 		this._db = this._client.db(DATABASE_NAME);
 
-		//#region Accounts
-		this._accounts = this._db.collection(ACCOUNTS_COLLECTION_NAME);
+		let migration: DbAutomaticMigration | undefined;
+		if (!inMemory || dbPath) {
+			if (DATABASE_MIGRATION !== 'disable') {
+				migration = {
+					dryRun: DATABASE_MIGRATION !== 'migrate',
+					log: logger.prefixMessages('Migration:'),
+					startTime: Date.now(),
+					success: true,
+					totalCount: 0,
+					changeCount: 0,
+				};
+				migration.log.alert(`Running database migration!${migration.dryRun ? ' (dry run)' : ''}`);
+			}
+		}
 
-		await MongoUpdateIndexes(this._accounts, [
-			{
-				name: 'id',
-				unique: true,
-				key: { id: 1 },
-			},
-			{
-				name: 'username',
-				unique: true,
-				key: { username: 1 },
-				// Usernames are case-insensitive
-				collation: COLLATION_CASE_INSENSITIVE,
-			},
-			{
-				name: 'emailHash',
-				unique: true,
-				key: { 'secure.emailHash': 1 },
-			},
-			{
-				name: 'githubId',
-				unique: true,
-				key: { 'secure.github.id': 1 },
-				// Ignore accounts without github data
-				sparse: true,
-			},
-		]);
+		//#region Init Collections
 
-		const [maxAccountId] = await this._accounts.find().sort({ id: -1 }).limit(1).toArray();
-		this._nextAccountId = maxAccountId ? maxAccountId.id + 1 : 1;
+		this._accounts = await accountCollection.create(this._db, migration);
+		this._nextAccountId = await accountCollection.max('id', 0) + 1;
+
+		this._characters = await characterCollection.create(this._db, migration);
+		this._nextCharacterId = await characterCollection.max('id', 0) + 1;
+
+		this._accountContacts = await accountContactCollection.create(this._db, migration);
+		this._spaces = await spaceCollection.create(this._db, migration);
+		this._config = await configCollection.create(this._db, migration);
+		this._directMessages = await directMessageCollection.create(this._db, migration);
+
 		//#endregion
 
-		//#region Characters
-		this._characters = this._db.collection(CHARACTERS_COLLECTION_NAME);
-
-		await MongoUpdateIndexes(this._characters, [
-			{
-				name: 'id',
-				unique: true,
-				key: { id: 1 },
-			},
-		]);
-
-		const [maxCharId] = await this._characters.find().sort({ id: -1 }).limit(1).toArray();
-		this._nextCharacterId = maxCharId ? maxCharId.id + 1 : 1;
-		//#endregion
-
-		this._accountContacts = this._db.collection(ACCOUNT_CONTACTS_COLLECTION_NAME);
-		await MongoUpdateIndexes(this._accountContacts, [
-			{
-				name: 'accounts',
-				key: { accounts: 1 },
-			},
-		]);
-
-		//#region Spaces
-		this._spaces = this._db.collection(SPACES_COLLECTION_NAME);
-
-		await this._spaces.createIndexes([
-			{
-				name: 'id',
-				unique: true,
-				key: { id: 1 },
-			},
-		]);
-		//#endregion
-
-		//#region Config
-		this._config = this._db.collection('config');
-
-		await MongoUpdateIndexes(this._config, [
-			{
-				name: 'id',
-				unique: true,
-				key: { type: 1 },
-			},
-		]);
-		//#endregion
-
-		//#region DirectMessages
-		this._directMessages = this._db.collection(DIRECT_MESSAGES_COLLECTION_NAME);
-
-		await MongoUpdateIndexes(this._directMessages, [
-			{
-				name: 'accounts',
-				key: { accounts: 1 },
-			},
-			{
-				name: 'time',
-				key: { time: 1 },
-			},
-		]);
-		//#endregion
+		if (migration) {
+			if (!migration.success) {
+				migration.log.fatal('Database migration failed.');
+				throw new Error('Database migration failed');
+			}
+			const endTime = Date.now();
+			migration.log.alert(`Database migration completed in ${endTime - migration.startTime}ms, processed ${migration.totalCount} documents, updated ${migration.changeCount} documents${migration.dryRun ? ' (dry run)' : ''}`);
+		}
 
 		logger.info(`Initialized ${this._inMemoryServer ? 'In-Memory-' : ''}MongoDB database`);
-
-		if (!inMemory || dbPath) {
-			await this._doMigrations();
-		}
 	}
 
 	public async onDestroy(): Promise<void> {
@@ -519,140 +554,6 @@ export default class MongoDatabase implements PandoraDatabase {
 	public async setConfig<T extends DatabaseConfigType>(type: T, data: DatabaseConfigData<T>): Promise<void> {
 		await this._config.updateOne({ type }, { $set: { data } }, { upsert: true });
 	}
-
-	private async _doMigrations(): Promise<void> {
-		if (DATABASE_MIGRATION === 'disable')
-			return;
-
-		const dryRun = DATABASE_MIGRATION !== 'migrate';
-		const migrationLog = logger.prefixMessages('Migration:');
-
-		migrationLog.alert(`Running database migration!${dryRun ? ' (dry run)' : ''}`);
-
-		// Some stats to collect throughout migration
-		const startTime = Date.now();
-		let success = true;
-		let totalCount = 0;
-		let changeCount = 0;
-
-		// A generic function that processes a single collection, applying a schema to each document in the collection
-		const performMigration = async <T extends object>(name: string, collection: Collection<T>, schema: ZodType<T, ZodTypeDef, unknown>): Promise<void> => {
-			migrationLog.info(`Processing ${name}...`);
-
-			for await (const originalData of collection.find().stream()) {
-				totalCount++;
-				const documentId: ObjectId = originalData._id;
-				Assert(documentId instanceof ObjectId);
-
-				const originalDataWithoutType: Record<string, unknown> = originalData;
-				// Parse the data using schema (this is the main bit of migration)
-				const parsedData = schema.safeParse(originalData);
-				if (!parsedData.success) {
-					success = false;
-					migrationLog.error(`Failed to migrate ${name} document ${documentId.toHexString()}:\n`, parsedData.error);
-					continue;
-				}
-
-				if (isEqual(omit(originalData, '_id'), omit(parsedData.data, '_id')))
-					continue;
-
-				// If the parse result is not equal, the data needs migration
-				changeCount++;
-
-				// Collect updated properties and keys to delete
-				const update: Partial<T> = {};
-				const keysToDelete = new Set<string>();
-				for (const [key, value] of KnownObject.entries(parsedData.data)) {
-					if (key === '_id')
-						continue;
-
-					if (value !== undefined && !isEqual(originalDataWithoutType[key as string], value)) {
-						update[key] = value;
-					}
-				}
-				for (const key of Object.keys(originalData)) {
-					if (key === '_id')
-						continue;
-
-					if (parsedData.data[key as keyof T] === undefined) {
-						keysToDelete.add(key);
-					}
-				}
-
-				// Generate a diff for manual review
-				const diff = diffString(omit(originalData, '_id'), omit(parsedData.data, '_id'), { color: false });
-				migrationLog.verbose(
-					`Migrating ${name} document ${documentId.toHexString()}...\n`,
-					`Updates keys: ${Object.keys(update).join(', ')}\n`,
-					`Removed keys: ${Array.from(keysToDelete).join(', ')}\n`,
-					`Diff:\n`,
-					diff,
-				);
-
-				if (dryRun)
-					continue;
-
-				// Actually perform the update
-				const { matchedCount } = await collection.updateOne(
-					// @ts-expect-error: This works; typechecking is just broken for some reason
-					{ _id: documentId },
-					{
-						$set: update,
-						$unset: ArrayToRecordKeys(Array.from(keysToDelete), true),
-					},
-				);
-				if (matchedCount !== 1) {
-					success = false;
-					migrationLog.error(`Failed to migrate ${name} document ${documentId.toHexString()}: Update matched count is ${matchedCount}`);
-				}
-			}
-		};
-
-		// Perform migration on all collections
-		await performMigration<DatabaseAccountWithSecure>(
-			'accounts',
-			this._accounts,
-			DatabaseAccountWithSecureSchema,
-		);
-
-		await performMigration<Omit<ICharacterData, 'id'> & { id: number; }>(
-			'characters',
-			this._characters,
-			CharacterDataSchema.omit({ id: true }).and(z.object({ id: z.number().int() })),
-		);
-
-		await performMigration<DatabaseAccountContact>(
-			'accountContacts',
-			this._accountContacts,
-			ZodCast<DatabaseAccountContact>(), // TODO: This needs a proper schema!
-		);
-
-		await performMigration<SpaceData>(
-			'spaces',
-			this._spaces,
-			SpaceDataSchema,
-		);
-
-		await performMigration<{ type: DatabaseConfigType; data: DatabaseConfigData<DatabaseConfigType>; }>(
-			'config',
-			this._config,
-			ZodCast<{ type: DatabaseConfigType; data: DatabaseConfigData<DatabaseConfigType>; }>(), // TODO: This needs a proper schema!
-		);
-
-		await performMigration<IDirectoryDirectMessage & { accounts: DirectMessageAccounts; }>(
-			'directMessages',
-			this._directMessages,
-			ZodCast<IDirectoryDirectMessage & { accounts: DirectMessageAccounts; }>(), // TODO: This needs a proper schema!
-		);
-
-		if (!success) {
-			migrationLog.fatal('Database migration failed.');
-			throw new Error('Database migration failed');
-		}
-
-		const endTime = Date.now();
-		migrationLog.alert(`Database migration completed in ${endTime - startTime}ms, processed ${totalCount} documents, updated ${changeCount} documents${dryRun ? ' (dry run)' : ''}`);
-	}
 }
 
 async function CreateInMemoryMongo({
@@ -687,72 +588,4 @@ function Id(obj: Omit<ICharacterData, 'id'> & { id: number; }): ICharacterData {
 
 function PlainId(id: CharacterId): number {
 	return parseInt(id.slice(1));
-}
-
-/**
- * Updates indexes on a collection such that they exactly match the wanted indexes, dropping all other indexes and updating existing ones
- *
- * Ignores the inbuilt `_id_` index
- * @param collection - The collection to update indexes on
- * @param indexes - The wanted indexes
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function MongoUpdateIndexes(collection: Collection<any>, indexes: (IndexDescription & { name: string; })[]): Promise<void> {
-	// Keys that should be compared
-	const indexKeysToCompare: readonly (keyof IndexDescription)[] = ['unique', 'sparse', 'key', 'collation'];
-
-	// We catch the result and return empty index array in case the collection doesn't exist
-	const currentIndexes: unknown[] = await collection.listIndexes().toArray().catch(() => []);
-
-	let rebuildNeeded = false;
-	// Check if there is any index that is different and needs rebuild
-	for (const index of currentIndexes) {
-		// Skip indexes in unknown format and inbuilt `_id_` index
-		if (!IsObject(index) || typeof index.name !== 'string' || index.name === '_id_')
-			continue;
-
-		// Check for non-existent indexes
-		const wantedIndex = indexes.find((i) => i.name === index.name);
-		if (!wantedIndex) {
-			rebuildNeeded = true;
-			logger.alert(`[Collection ${collection.collectionName}] Rebuilding indexes because of extra index:`, index.name);
-			break;
-		}
-
-		// Compare existing index to wanted one
-		for (const property of indexKeysToCompare) {
-			let matches = _.isEqual(index[property], wantedIndex[property]);
-			// Collation is compared only for partiality
-			if (!matches && property === 'collation' && wantedIndex.collation && IsObject(index.collation)) {
-				matches = true;
-				for (const k of Object.keys(wantedIndex.collation) as (keyof CollationOptions)[]) {
-					if (!_.isEqual(index.collation[k], wantedIndex.collation[k])) {
-						matches = false;
-						break;
-					}
-				}
-			}
-			if (!matches) {
-				rebuildNeeded = true;
-				logger.alert(`[Collection ${collection.collectionName}] Rebuilding indexes because of mismatched index '${index.name}' property '${property}'`);
-				break;
-			}
-		}
-		if (rebuildNeeded)
-			break;
-	}
-
-	if (rebuildNeeded) {
-		await collection.dropIndexes().catch(() => { /* NOOP */ });
-		if (indexes.length > 0) {
-			await collection.createIndexes(indexes);
-		}
-	} else {
-		// Check for new indexes if we didn't need complete rebuild
-		const newIndexes = indexes.filter((i) => !currentIndexes.some((ci) => IsObject(ci) && ci.name === i.name));
-		if (newIndexes.length > 0) {
-			logger.alert(`[Collection ${collection.collectionName}] Adding missing indexes:`, newIndexes.map((i) => i.name).join(', '));
-			await collection.createIndexes(newIndexes);
-		}
-	}
 }
---
+++ pandora-server-directory/src/database/validatedCollection.ts
@@ -0,0 +1,194 @@
+import type { ZodType, ZodTypeDef } from 'zod';
+import type { ConditionalKeys } from 'type-fest';
+import { diffString } from 'json-diff';
+import { isEqual, omit } from 'lodash';
+import { Collection, Db, Document, IndexDescription, ObjectId, WithId, type CollationOptions } from 'mongodb';
+import { ArrayToRecordKeys, Assert, IsObject, KnownObject, Logger } from 'pandora-common';
+
+export interface DbAutomaticMigration {
+	readonly dryRun: boolean;
+	readonly log: Logger;
+	readonly startTime: number;
+	success: boolean;
+	totalCount: number;
+	changeCount: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ValidatedCollectionType<T extends ValidatedCollection<any>> = T extends ValidatedCollection<infer U> ? Collection<U> : never;
+
+export class ValidatedCollection<T extends Document> {
+	public readonly logger: Logger;
+	public readonly name: string;
+	public readonly schema: ZodType<T, ZodTypeDef, unknown>;
+	public readonly indexes: (IndexDescription & { name: string; })[];
+	private collection!: Collection<T>;
+
+	constructor(
+		logger: Logger,
+		name: string,
+		schema: ZodType<T, ZodTypeDef, unknown>,
+		indexes: (IndexDescription & { name: string; })[],
+	) {
+		this.logger = logger;
+		this.name = name;
+		this.schema = schema;
+		this.indexes = indexes;
+	}
+
+	public async max<TKey extends ConditionalKeys<WithId<T>, number> & string>(key: TKey, fallback: WithId<T>[TKey]): Promise<WithId<T>[TKey]> {
+		const max = await this.collection.find().sort({ [key]: -1 }).limit(1).toArray();
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+		return max.length > 0 ? max[0][key] : fallback;
+	}
+
+	public async create(db: Db, migration?: DbAutomaticMigration): Promise<Collection<T>> {
+		if (this.collection == null) {
+			this.collection = db.collection(this.name);
+			await this.updateIndexes(this.collection);
+		}
+		if (migration != null) {
+			await this.automaticMigration(migration);
+		}
+		return this.collection;
+	}
+
+	/**
+	 * Updates indexes on a collection such that they exactly match the wanted indexes, dropping all other indexes and updating existing ones
+	 *
+	 * Ignores the inbuilt `_id_` index
+	 * @param collection - The collection to update indexes on
+	 */
+	public async updateIndexes(collection: Collection<T>): Promise<void> {
+		// Keys that should be compared
+		const indexKeysToCompare: readonly (keyof IndexDescription)[] = ['unique', 'sparse', 'key', 'collation'];
+
+		// We catch the result and return empty index array in case the collection doesn't exist
+		const currentIndexes: unknown[] = await collection.listIndexes().toArray().catch(() => []);
+
+		let rebuildNeeded = false;
+		// Check if there is any index that is different and needs rebuild
+		for (const index of currentIndexes) {
+			// Skip indexes in unknown format and inbuilt `_id_` index
+			if (!IsObject(index) || typeof index.name !== 'string' || index.name === '_id_')
+				continue;
+
+			// Check for non-existent indexes
+			const wantedIndex = this.indexes.find((i) => i.name === index.name);
+			if (!wantedIndex) {
+				rebuildNeeded = true;
+				this.logger.alert(`[Collection ${collection.collectionName}] Rebuilding indexes because of extra index:`, index.name);
+				break;
+			}
+
+			// Compare existing index to wanted one
+			for (const property of indexKeysToCompare) {
+				let matches = isEqual(index[property], wantedIndex[property]);
+				// Collation is compared only for partiality
+				if (!matches && property === 'collation' && wantedIndex.collation && IsObject(index.collation)) {
+					matches = true;
+					for (const k of Object.keys(wantedIndex.collation) as (keyof CollationOptions)[]) {
+						if (!isEqual(index.collation[k], wantedIndex.collation[k])) {
+							matches = false;
+							break;
+						}
+					}
+				}
+				if (!matches) {
+					rebuildNeeded = true;
+					this.logger.alert(`[Collection ${collection.collectionName}] Rebuilding indexes because of mismatched index '${index.name}' property '${property}'`);
+					break;
+				}
+			}
+			if (rebuildNeeded)
+				break;
+		}
+
+		if (rebuildNeeded) {
+			await collection.dropIndexes().catch(() => { /* NOOP */ });
+			if (this.indexes.length > 0) {
+				await collection.createIndexes(this.indexes);
+			}
+		} else {
+			// Check for new indexes if we didn't need complete rebuild
+			const newIndexes = this.indexes.filter((i) => !currentIndexes.some((ci) => IsObject(ci) && ci.name === i.name));
+			if (newIndexes.length > 0) {
+				this.logger.alert(`[Collection ${collection.collectionName}] Adding missing indexes:`, newIndexes.map((i) => i.name).join(', '));
+				await collection.createIndexes(newIndexes);
+			}
+		}
+	}
+
+	private async automaticMigration(migration: DbAutomaticMigration): Promise<void> {
+		const { log, dryRun } = migration;
+		log.info(`Processing ${this.name}...`);
+
+		for await (const originalData of this.collection.find().stream()) {
+			migration.totalCount++;
+			const documentId: ObjectId = originalData._id;
+			Assert(documentId instanceof ObjectId);
+
+			const originalDataWithoutType: Record<string, unknown> = originalData;
+			// Parse the data using schema (this is the main bit of migration)
+			const parsedData = this.schema.safeParse(originalData);
+			if (!parsedData.success) {
+				migration.success = false;
+				log.error(`Failed to migrate ${this.name} document ${documentId.toHexString()}:\n`, parsedData.error);
+				continue;
+			}
+
+			if (isEqual(omit(originalData, '_id'), omit(parsedData.data, '_id')))
+				continue;
+
+			// If the parse result is not equal, the data needs migration
+			migration.changeCount++;
+
+			// Collect updated properties and keys to delete
+			const update: Partial<T> = {};
+			const keysToDelete = new Set<string>();
+			for (const [key, value] of KnownObject.entries(parsedData.data)) {
+				if (key === '_id')
+					continue;
+
+				if (value !== undefined && !isEqual(originalDataWithoutType[key as string], value)) {
+					update[key] = value;
+				}
+			}
+			for (const key of Object.keys(originalData)) {
+				if (key === '_id')
+					continue;
+
+				if (parsedData.data[key as keyof T] === undefined) {
+					keysToDelete.add(key);
+				}
+			}
+
+			// Generate a diff for manual review
+			const diff = diffString(omit(originalData, '_id'), omit(parsedData.data, '_id'), { color: false });
+			log.verbose(
+				`Migrating ${this.name} document ${documentId.toHexString()}...\n`,
+				`Updates keys: ${Object.keys(update).join(', ')}\n`,
+				`Removed keys: ${Array.from(keysToDelete).join(', ')}\n`,
+				`Diff:\n`,
+				diff,
+			);
+
+			if (dryRun)
+				continue;
+
+			// Actually perform the update
+			const { matchedCount } = await this.collection.updateOne(
+				// @ts-expect-error: This works; typechecking is just broken for some reason
+				{ _id: documentId },
+				{
+					$set: update,
+					$unset: ArrayToRecordKeys(Array.from(keysToDelete), true),
+				},
+			);
+			if (matchedCount !== 1) {
+				migration.success = false;
+				log.error(`Failed to migrate ${this.name} document ${documentId.toHexString()}: Update matched count is ${matchedCount}`);
+			}
+		}
+	}
+}

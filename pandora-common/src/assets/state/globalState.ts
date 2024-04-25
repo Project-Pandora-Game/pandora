acff6897bce3183b9e4d49193314a5dac4059b37
Jomshir98 <jomshir98@protonmail.com>
2024-04-25 18:56:54 +0200

    Revert "[FIX] Clear occupied slots after character leaves space (#722)"
    This reverts commit 3d08266c5e2d6727652fea38a3c997ecc5217ef4.

--- pandora-common/src/assets/state/globalState.ts
+++ pandora-common/src/assets/state/globalState.ts
@@ -145,7 +145,12 @@ export class AssetFrameworkGlobalState {
 
 	public withRoomState(newState: AssetFrameworkRoomState): AssetFrameworkGlobalState {
 		const newCharacters = new Map(this.characters);
-		this._updateRoomStateLinkOnCharacters(newCharacters, newState);
+		for (const [id, character] of newCharacters) {
+			newCharacters.set(
+				id,
+				character.updateRoomStateLink(newState, false),
+			);
+		}
 
 		return new AssetFrameworkGlobalState(
 			this.assetManager,
@@ -156,14 +161,9 @@ export class AssetFrameworkGlobalState {
 
 	public withCharacter(characterId: CharacterId, characterState: AssetFrameworkCharacterState | null): AssetFrameworkGlobalState {
 		const newCharacters = new Map(this.characters);
-		let newRoom = this.room;
 
 		if (characterState == null) {
 			newCharacters.delete(characterId);
-			newRoom = newRoom.clearSlotsOccupiedByCharacter(characterId);
-			if (newRoom !== this.room) {
-				this._updateRoomStateLinkOnCharacters(newCharacters, newRoom);
-			}
 		} else {
 			Assert(characterId === characterState.id);
 			newCharacters.set(characterId, characterState);
@@ -172,7 +172,7 @@ export class AssetFrameworkGlobalState {
 		return new AssetFrameworkGlobalState(
 			this.assetManager,
 			newCharacters,
-			newRoom,
+			this.room,
 		);
 	}
 
@@ -236,15 +236,6 @@ export class AssetFrameworkGlobalState {
 		Assert(resultState.isValid(), 'State is invalid after load');
 
 		return resultState;
-	}
-
-	private _updateRoomStateLinkOnCharacters(characters: Map<CharacterId, AssetFrameworkCharacterState>, roomState: AssetFrameworkRoomState) {
-		for (const [id, character] of characters) {
-			characters.set(
-				id,
-				character.updateRoomStateLink(roomState, false),
-			);
-		}
 	}
 }
 

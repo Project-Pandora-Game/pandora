49e6946588a682b68ac8a553a8baa71e171015d9
jomshir98 <jomshir98@protonmail.com>
2024-05-14 16:14:46 +0000

    [ADD] Allow dynamic option names and allow specifying order

--- pandora-client-web/src/components/settings/helpers/settingsInputs.tsx
+++ pandora-client-web/src/components/settings/helpers/settingsInputs.tsx
@@ -60,11 +60,12 @@ export function ToggleSettingInput({ currentValue, defaultValue, label, onChange
 	);
 }
 
-export function SelectSettingInput<TValue extends string>({ currentValue, defaultValue, label, stringify, schema, onChange, onReset, deps }: {
+export function SelectSettingInput<TValue extends string>({ currentValue, defaultValue, label, stringify, optionOrder, schema, onChange, onReset, deps }: {
 	currentValue: TValue | undefined;
 	defaultValue: TValue;
 	label: ReactNode;
-	stringify: Readonly<Record<TValue, string>>;
+	stringify: Readonly<Record<TValue, string | (() => string)>>;
+	optionOrder?: readonly TValue[];
 	schema: ZodSchema<TValue, ZodTypeDef, unknown>;
 	onChange: (newValue: TValue) => void;
 	onReset?: () => void;
@@ -91,15 +92,19 @@ export function SelectSettingInput<TValue extends string>({ currentValue, defaul
 		setValue(newValue);
 	}, [schema, setValue]);
 
-	const options = useMemo(() => (
-		KnownObject
-			.entries(stringify)
-			.map(([k, v]) => (
-				<option key={ k } value={ k }>
-					{ v }
-				</option>
-			))
-	), [stringify]);
+	const options = useMemo(() => {
+		const entries = KnownObject.entries(stringify);
+
+		if (optionOrder != null) {
+			entries.sort((a, b) => optionOrder.indexOf(a[0]) - optionOrder.indexOf(b[0]));
+		}
+
+		return entries.map(([k, v]) => (
+			<option key={ k } value={ k }>
+				{ typeof v === 'function' ? v() : v }
+			</option>
+		));
+	}, [stringify, optionOrder]);
 
 	return (
 		<div className='input-section'>

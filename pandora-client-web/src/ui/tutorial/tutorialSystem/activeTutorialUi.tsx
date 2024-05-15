a60517acb1bcc7ee8588153ff3f86cc280b5ff66
jomshir98 <jomshir98@protonmail.com>
2024-05-15 18:28:44 +0000

    [ADD] Highlight action targets

--- pandora-client-web/src/ui/tutorial/tutorialSystem/activeTutorialUi.tsx
+++ pandora-client-web/src/ui/tutorial/tutorialSystem/activeTutorialUi.tsx
@@ -1,11 +1,11 @@
 import type { Immutable } from 'immer';
-import { AssertNever } from 'pandora-common';
-import React, { useEffect, type ReactElement } from 'react';
+import { AssertNever, EMPTY_ARRAY } from 'pandora-common';
+import React, { useCallback, useEffect, useState, type ReactElement } from 'react';
 import { Button } from '../../../components/common/button/button';
 import { Column, Row } from '../../../components/common/container/container';
-import { DraggableDialog } from '../../../components/dialog/dialog';
+import { DialogInPortal, DraggableDialog } from '../../../components/dialog/dialog';
 import { useObservable } from '../../../observable';
-import type { TutorialCondition, TutorialStep } from './tutorialConfig';
+import type { TutorialCondition, TutorialHighlightSelector, TutorialStep } from './tutorialConfig';
 import type { TutorialRunner, TutorialStageRunner } from './tutorialRunner';
 import './tutorialUi.scss';
 
@@ -98,6 +98,13 @@ function ActiveTutorialStepUi({ stage, step, stepIndex, active }: {
 				) : null
 			}
 			{
+				step.highlight?.map((highlight, index) => (
+					<ActiveTutorialHighlight key={ index }
+						highlight={ highlight }
+					/>
+				))
+			}
+			{
 				step.conditions.map((condition, conditionIndex) => (
 					<ActiveTutorialStepCondition key={ conditionIndex }
 						stage={ stage }
@@ -117,6 +124,8 @@ function ActiveTutorialStepCondition({ condition, stage, stepIndex }: {
 	step: Immutable<TutorialStep>;
 	condition: Immutable<TutorialCondition>;
 }): ReactElement | null {
+	const [highlightElement, setHighlightElement] = useState<HTMLElement | null>(null);
+
 	if (condition.type === 'next') {
 
 		return (
@@ -126,9 +135,15 @@ function ActiveTutorialStepCondition({ condition, stage, stepIndex }: {
 					onClick={ () => {
 						stage.stepClickNext(stepIndex);
 					} }
+					ref={ setHighlightElement }
 				>
 					Next { '\u25b8' }
 				</Button>
+				{
+					highlightElement != null ? (
+						<ActiveTutorialElementHighlight target={ highlightElement } />
+					) : null
+				}
 			</Row>
 		);
 	} else if (condition.type === 'url') {
@@ -140,4 +155,96 @@ function ActiveTutorialStepCondition({ condition, stage, stepIndex }: {
 	}
 
 	AssertNever(condition);
+}
+
+const HIGHLIGHT_QUERY_INTERVAL = 1_000;
+const HIGHLIGHT_POSITION_INTERVAL = 200;
+const HIGHLIGHT_PADDING = 8;
+
+function ActiveTutorialHighlight({ highlight }: {
+	highlight: TutorialHighlightSelector;
+}): ReactElement {
+	const [elements, setElements] = useState<readonly HTMLElement[]>(EMPTY_ARRAY);
+
+	const update = useCallback(() => {
+		const newElements = Array.from(document.querySelectorAll(highlight.query))
+			.filter((e): e is HTMLElement => e instanceof HTMLElement)
+			.filter((e) => {
+				if (highlight.filter == null)
+					return true;
+
+				return highlight.filter(e);
+			});
+
+		setElements(newElements);
+	}, [highlight]);
+
+	useEffect(() => {
+		const interval = setInterval(update, HIGHLIGHT_QUERY_INTERVAL);
+		update();
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [update]);
+
+	return (
+		<>
+			{
+				elements.map((el, i) => (
+					<ActiveTutorialElementHighlight key={ el.id || i }
+						target={ el }
+					/>
+				))
+			}
+		</>
+	);
+}
+
+function ActiveTutorialElementHighlight({ target }: {
+	target: HTMLElement;
+}): ReactElement | null {
+	const [area, setArea] = useState<[number, number, number, number]>([0, 0, 0, 0]);
+
+	const update = useCallback(() => {
+		if (target.offsetParent == null) {
+			setArea([0, 0, 0, 0]);
+			return;
+		}
+
+		const bounds = target.getBoundingClientRect();
+		setArea([bounds.left, bounds.top, bounds.width, bounds.height]);
+	}, [target]);
+
+	useEffect(() => {
+		const resizeObserver = new ResizeObserver(update);
+		resizeObserver.observe(target);
+
+		// Update on timer too, because resize observer doesn't catch moves
+		const interval = setInterval(update, HIGHLIGHT_POSITION_INTERVAL);
+
+		update();
+
+		return () => {
+			clearInterval(interval);
+			resizeObserver.disconnect();
+		};
+	}, [target, update]);
+
+	if (area[2] === 0 || area[3] === 0)
+		return null;
+
+	return (
+		<DialogInPortal>
+			<div
+				className='tutorial-highlight-overlay'
+				style={ {
+					left: area[0] - HIGHLIGHT_PADDING,
+					top: area[1] - HIGHLIGHT_PADDING,
+					width: area[2] + 2 * HIGHLIGHT_PADDING,
+					height: area[3] + 2 * HIGHLIGHT_PADDING,
+				} }
+			/>
+		</DialogInPortal>
+	);
 }

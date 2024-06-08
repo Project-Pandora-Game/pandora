d025f1cd3c57d6e38503ae240665695b6437a9e5
jomshir98 <jomshir98@protonmail.com>
2024-06-08 09:42:01 +0000

    [ADD] Point template delta draft

---
+++ pandora-common/src/assets/graphics/points_delta.ts
@@ -0,0 +1,32 @@
+import type { Immutable } from 'immer';
+import { isEqual } from 'lodash';
+import { z } from 'zod';
+import { CloneDeepMutable } from '../../utility';
+import { CoordinatesCompressedSchema } from './common';
+import { PointDefinitionSchema, type PointTemplate } from './points';
+
+export const PointTemplateDeltaSchema = z.object({
+	removed: CoordinatesCompressedSchema.array().optional(),
+	added: PointDefinitionSchema.array().optional(),
+});
+export type PointTemplateDelta = z.infer<typeof PointTemplateDeltaSchema>;
+
+export function PointTemplateDiff(base: Immutable<PointTemplate>, target: Immutable<PointTemplate>): PointTemplateDelta {
+	const result: PointTemplateDelta = {};
+
+	for (const basePoint of base) {
+		if (!target.some((targetPoint) => isEqual(basePoint, targetPoint))) {
+			result.removed ??= [];
+			result.removed.push(CloneDeepMutable(basePoint.pos));
+		}
+	}
+
+	for (const targetPoint of target) {
+		if (!base.some((basePoint) => isEqual(basePoint, targetPoint))) {
+			result.added ??= [];
+			result.added.push(CloneDeepMutable(targetPoint));
+		}
+	}
+
+	return result;
+}

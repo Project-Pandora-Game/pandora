d0bfcf129a279e163704dedabb6861d4a03ee260
jomshir98 <jomshir98@protonmail.com>
2024-07-17 22:19:16 +0000

    [DEV] Document tick hook

--- pandora-client-web/src/graphics/reconciler/tick.ts
+++ pandora-client-web/src/graphics/reconciler/tick.ts
@@ -2,6 +2,11 @@ import type { Ticker } from 'pixi.js';
 import { useEffect } from 'react';
 import { usePixiAppOptional } from './appContext';
 
+/**
+ * Adds a tick handler that is called each time a Pixi tick happens (so each time there is a render of the tree)
+ * @param callback - The callback that should be called
+ * @param enabled - Whether this callback is enabled or not (allowing to disable the tick without calling the hook conditionally)
+ */
 export function usePixiTick(callback: (dt: number, ticker: Ticker) => void, enabled: boolean = true) {
 	const app = usePixiAppOptional();
 

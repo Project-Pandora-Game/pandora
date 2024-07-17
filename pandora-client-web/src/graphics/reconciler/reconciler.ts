2892da9d5057516600169db0b0b2bac06a207251
jomshir98 <jomshir98@protonmail.com>
2024-07-17 22:17:38 +0000

    [DEV] Document reconciler

--- pandora-client-web/src/graphics/reconciler/reconciler.ts
+++ pandora-client-web/src/graphics/reconciler/reconciler.ts
@@ -7,6 +7,7 @@ import { GAME_VERSION, NODE_ENV } from '../../config/Environment';
 import { PixiRootContainer, type PixiUpdateEmitter } from './element';
 import { PIXI_FIBER_HOST_CONFIG } from './reconciler-config';
 
+/** React reconciler instance with config for working with Pixi elements. */
 export const PixiFiber = ReactReconciler(PIXI_FIBER_HOST_CONFIG);
 
 // Inject our fiber into devtools for both debugging and HMR support
@@ -16,21 +17,27 @@ PixiFiber.injectIntoDevTools({
 	rendererPackageName: 'pandora-client-web/pixi-renderer',
 });
 
+/** A root handle for working with a Pixi React root. */
 export type PixiRoot = {
+	/** Render element into the root, replacing any existing content. */
 	render: (element: ReactNode) => void;
+	/** Unmount the root - removing all elements and performing any necessary cleanup. */
 	unmount: () => void;
+	/** Emitter used for delivering update events for lazy rendering. */
 	updateEmitter: PixiUpdateEmitter;
 };
 
 /**
  * Create a rendered on top of a PIXI Container, ready to render react content.
- * @param rootContainer - Container to create the root on
- * @returns {{ render: Function, unmount: Function}}
+ * @param rootContainer - Container to create the root on; the container must be empty.
+ * @returns A handle for the newly created root.
  */
 export function CreatePixiRoot(rootContainer: Container): PixiRoot {
 	Assert(rootContainer.children.length === 0, 'Cannot create PixiRoot on a container that already has children. React must manage all the children itself.');
+	// Wrap the root
 	const root = new PixiRootContainer(rootContainer);
 
+	// Create a React container for the root
 	const container: unknown = PixiFiber.createContainer(
 		root,
 		ConcurrentRoot,
@@ -44,6 +51,7 @@ export function CreatePixiRoot(rootContainer: Container): PixiRoot {
 		null,
 	);
 
+	// Return a handle for the root, allowing it to be manipulated.
 	return {
 		render(element) {
 			PixiFiber.updateContainer(element, container);

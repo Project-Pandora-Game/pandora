f81430bb94e85113e76e94c3132e8d7bed9ec2b8
jomshir98 <jomshir98@protonmail.com>
2024-09-13 18:14:58 +0000

    [ADD] Tabs styling cleanup and theming support

--- pandora-client-web/src/components/common/tabs/tabs.tsx
+++ pandora-client-web/src/components/common/tabs/tabs.tsx
@@ -20,7 +20,7 @@ interface TabConfig {
 	tabClassName?: string;
 }
 
-export function Tabulation({ children, className, collapsable, tabsPosition, tabs }: {
+export function Tabulation({ children, className, collapsable, tabsPosition = 'top', tabs }: {
 	children: ReactNode;
 	className?: string;
 	collapsable?: true;
@@ -35,12 +35,14 @@ export function Tabulation({ children, className, collapsable, tabsPosition, tab
 
 	return (
 		<div className={ classNames('tab-container', `tab-position-${tabsPosition}`, className) }>
-			<ul className={ classNames('header', { collapsed }) }>
+			<ul className={ classNames('header', { collapsed }) } role='tablist' aria-orientation={ tabsPosition === 'left' ? 'vertical' : 'horizontal' }>
 				{
 					tabs.map((tab, index) => (tab &&
 						<button key={ index }
 							className={ classNames('tab', { active: tab.active }, tab.tabClassName) }
 							onClick={ tab.onClick }
+							role='tab'
+							aria-selected={ tab.active }
 						>
 							{ tab.name }
 						</button>
@@ -48,16 +50,16 @@ export function Tabulation({ children, className, collapsable, tabsPosition, tab
 				}
 				{
 					collapsable && (
-						<li className='tab collapse' onClick={ () => setCollapsed(true) }>
+						<button className='tab collapse' onClick={ () => setCollapsed(true) } title='Hide tabs'>
 							▲
-						</li>
+						</button>
 					)
 				}
 			</ul>
 			{ !collapsed ? null : (
-				<div className='tab-container-collapsed' onClick={ () => setCollapsed(false) }>
+				<button className='tab-container-collapsed' onClick={ () => setCollapsed(false) } title='Reveal hidden tabs'>
 					▼
-				</div>
+				</button>
 			) }
 			{ children }
 		</div>

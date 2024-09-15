27931bd55c7146a9ddd479b251b918290cead0cd
jomshir98 <jomshir98@protonmail.com>
2024-09-15 21:32:30 +0000

    [CHANGE] Unroll "Slots" menu for devices

--- pandora-client-web/src/graphics/room/contextMenus/deviceContextMenu.tsx
+++ pandora-client-web/src/graphics/room/contextMenus/deviceContextMenu.tsx
@@ -6,7 +6,7 @@ import React, { ReactElement, useCallback, useEffect, useMemo, useState } from '
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import type { z } from 'zod';
-import { ICharacter, useCharacterData } from '../../../character/character';
+import { ICharacter, useCharacterData, useCharacterDataOptional } from '../../../character/character';
 import { ChildrenProps } from '../../../common/reactTypes';
 import { Column } from '../../../components/common/container/container';
 import { Scrollable } from '../../../components/common/scrollbar/scrollbar';
@@ -178,15 +178,16 @@ function OccupyDeviceSlotMenu({ device, slot, character, close }: {
 	);
 }
 
-function DeviceSlotsMenu({ device, position, close }: {
+function DeviceSlotMenu({ slot, device, position, closeSlot }: {
+	slot: string;
 	device: ItemRoomDevice;
 	position: Readonly<PointLike>;
-	close: () => void;
+	closeSlot: () => void;
 }) {
-	const [slot, setSlot] = useState<string | null>(null);
-	const occupancy = useMemo(() => slot && device.slotOccupancy.get(slot), [device, slot]);
+	const occupancy = useMemo(() => device.slotOccupancy.get(slot), [device, slot]);
 	const characters = useSpaceCharacters();
 	const character = useMemo(() => characters.find(({ id }) => id === occupancy), [characters, occupancy]);
+	const characterData = useCharacterDataOptional(character ?? null);
 
 	const {
 		openContextMenu,
@@ -199,19 +200,6 @@ function DeviceSlotsMenu({ device, position, close }: {
 		openContextMenu(character, position);
 	}, [character, position, openContextMenu]);
 
-	if (!slot) {
-		return (
-			<>
-				<hr />
-				{ Object.entries(device.asset.definition.slots).map(([name, definition]) => (
-					<button key={ name } onClick={ () => setSlot(name) }>
-						{ definition.name }
-					</button>
-				)) }
-			</>
-		);
-	}
-
 	if (occupancy) {
 		return (
 			<>
@@ -219,8 +207,13 @@ function DeviceSlotsMenu({ device, position, close }: {
 					{ device.asset.definition.slots[slot].name }
 				</span>
 				<hr />
-				<button onClick={ onSelectCharacter }>
-					{ character?.name } ({ character?.id })
+				<button
+					onClick={ onSelectCharacter }
+					style={ {
+						backgroundColor: characterData != null ? `${characterData.settings.labelColor}44` : undefined,
+					} }
+				>
+					{ character?.name } ({ occupancy })
 				</button>
 				<hr />
 				<DeviceSlotClear device={ device } slot={ slot } close={ close }>
@@ -228,8 +221,8 @@ function DeviceSlotsMenu({ device, position, close }: {
 						? 'Exit the device'
 						: 'Clear occupancy of the slot' }
 				</DeviceSlotClear>
-				<button onClick={ () => setSlot(null) }>
-					Back to slots
+				<button onClick={ closeSlot }>
+					Back
 				</button>
 			</>
 		);
@@ -248,10 +241,44 @@ function DeviceSlotsMenu({ device, position, close }: {
 				<OccupyDeviceSlotMenu key={ char.id } device={ device } slot={ slot } character={ char } close={ close } />
 			)) }
 			<hr />
-			<button onClick={ () => setSlot(null) }>
-				Back to slots
+			<button onClick={ closeSlot }>
+				Back
 			</button>
 		</>
+	);
+}
+
+function DeviceMainMenu({ device, position, close }: {
+	device: ItemRoomDevice;
+	position: Readonly<PointLike>;
+	close: () => void;
+}) {
+	const [slot, setSlot] = useState<string | null>(null);
+
+	if (!slot) {
+		return (
+			<>
+				{ Object.keys(device.asset.definition.slots).length > 0 ? (<hr />) : null }
+				{ Object.entries(device.asset.definition.slots).map(([name, definition]) => (
+					<button key={ name } onClick={ () => setSlot(name) }>
+						{ definition.name }
+					</button>
+				)) }
+				<hr />
+				<LeaveDeviceMenu device={ device } close={ close } />
+				<MoveDeviceMenu device={ device } close={ close } />
+				<StoreDeviceMenu device={ device } close={ close } />
+			</>
+		);
+	}
+
+	return (
+		<DeviceSlotMenu
+			slot={ slot }
+			device={ device }
+			position={ position }
+			closeSlot={ () => setSlot(null) }
+		/>
 	);
 }
 
@@ -263,7 +290,7 @@ function DeviceContextMenuCurrent({ device, position, onClose }: {
 	const ref = useContextMenuPosition(position);
 	const player = usePlayer();
 	const gameState = useGameStateOptional();
-	const [menu, setMenu] = useState<'main' | 'slots'>('main');
+	const [menu, setMenu] = useState<'main'>('main');
 	const navigate = useNavigate();
 
 	const onCloseActual = useCallback(() => {
@@ -288,23 +315,11 @@ function DeviceContextMenuCurrent({ device, position, onClose }: {
 								{ device.asset.definition.name }
 							</button>
 							{ menu === 'main' && (
-								<>
-									<hr />
-									<LeaveDeviceMenu device={ device } close={ onClose } />
-									<button onClick={ () => setMenu('slots') }>
-										Slots
-									</button>
-									<MoveDeviceMenu device={ device } close={ onClose } />
-									<StoreDeviceMenu device={ device } close={ onClose } />
-								</>
-							) }
-							{ menu === 'slots' && (
-								<>
-									<DeviceSlotsMenu device={ device } position={ position } close={ onClose } />
-									<button onClick={ () => setMenu('main') }>
-										Back
-									</button>
-								</>
+								<DeviceMainMenu
+									device={ device }
+									position={ position }
+									close={ onCloseActual }
+								/>
 							) }
 						</WardrobeContextProvider>
 						<button onClick={ onClose } >

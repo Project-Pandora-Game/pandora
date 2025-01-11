10095935001819715dd183c34d9b0fb641a6ac73
jomshir98 <jomshir98@protonmail.com>
2025-01-11 21:07:26 +0000

    [ADD] Cleanup modifier definitions and add instance and effect types

--- pandora-common/src/gameLogic/characterModifiers/characterModifierData.ts
+++ pandora-common/src/gameLogic/characterModifiers/characterModifierData.ts
@@ -1,21 +1,51 @@
 import { z } from 'zod';
+import { KnownObject, ParseArrayNotEmpty } from '../../utility';
+import { RecordUnpackSubobjectProperties, ZodArrayWithInvalidDrop } from '../../validation';
 import { PermissionConfigSchema } from '../permissions';
+import { CharacterModifierTypeGenericIdSchema } from './characterModifierBaseData';
+import { CHARACTER_MODIFIER_TYPE_DEFINITION, type CharacterModifierType } from './modifierTypes/_index';
 
-export const CharacterModifierTypeGenericIdSchema = z.string();
-export type CharacterModifierTypeGenericId = z.infer<typeof CharacterModifierTypeGenericIdSchema>;
-
+/** Configuration for any character modifier _type_ (not an instance of a modifier) */
 export const CharacterModifierTypeConfigSchema = z.object({
 	permission: PermissionConfigSchema.nullable().catch(null),
 });
+/** Configuration for any character modifier _type_ (not an instance of a modifier) */
 export type CharacterModifierTypeConfig = z.infer<typeof CharacterModifierTypeConfigSchema>;
 
+/** Server-saved data of a character modifier instance */
+export const CharacterModifierInstanceDataSchema = z.discriminatedUnion('type',
+	ParseArrayNotEmpty(KnownObject.values(RecordUnpackSubobjectProperties('instanceDataSchema', CHARACTER_MODIFIER_TYPE_DEFINITION))),
+);
+/** Server-saved data of a character modifier instance (optionally filtered for a specific type) */
+export type CharacterModifierInstanceData<TType extends CharacterModifierType = CharacterModifierType> =
+	Extract<z.infer<typeof CharacterModifierInstanceDataSchema>, { readonly type: TType; }>;
+
+/** Data of modifier instance effect - put onto a character if the modifier is active */
+export const CharacterModifierEffectDataSchema = z.discriminatedUnion('type',
+	ParseArrayNotEmpty(KnownObject.values(RecordUnpackSubobjectProperties('effectDataSchema', CHARACTER_MODIFIER_TYPE_DEFINITION))),
+);
+/** Data of modifier instance effect - put onto a character if the modifier is active (optionally filtered for a specific type) */
+export type CharacterModifierEffectData<TType extends CharacterModifierType = CharacterModifierType> =
+	Extract<z.infer<typeof CharacterModifierEffectDataSchema>, { readonly type: TType; }>;
+
+/** Data of the whole character modifer subsystem, saved on character in database */
 export const CharacterModifierSystemDataSchema = z.object({
+	modifiers: ZodArrayWithInvalidDrop(CharacterModifierInstanceDataSchema, z.record(z.unknown())),
 	typeConfig: z.record(CharacterModifierTypeGenericIdSchema, CharacterModifierTypeConfigSchema.optional()),
 });
+/** Data of the whole character modifer subsystem, saved on character in database */
 export type CharacterModifierSystemData = z.infer<typeof CharacterModifierSystemDataSchema>;
 
+/** Create default data for character modifier subsystem */
 export function MakeDefaultCharacterModifierSystemData(): CharacterModifierSystemData {
 	return {
+		modifiers: [],
 		typeConfig: {},
 	};
+}
+
+/** Create an modifier effect from modifier instance */
+export function CharacterModifierInstanceToEffect<TType extends CharacterModifierType>(instanceData: CharacterModifierInstanceData<TType>): CharacterModifierEffectData<TType> {
+	// @ts-expect-error: Manually narrowed call.
+	return CHARACTER_MODIFIER_TYPE_DEFINITION[instanceData.type].instanceToEffect(instanceData);
 }
--- pandora-common/src/gameLogic/characterModifiers/helpers/modifierDefinition.ts
+++ pandora-common/src/gameLogic/characterModifiers/helpers/modifierDefinition.ts
@@ -1,11 +1,14 @@
-import type { z } from 'zod';
+import { cloneDeep } from 'lodash';
+import { z, type ZodObject } from 'zod';
 import type { EffectsDefinition } from '../../../assets/effects';
-import { AssertNever } from '../../../utility';
+import { Assert, AssertNever } from '../../../utility';
 import type { PermissionConfigDefault, PermissionType } from '../../permissions';
+import type { CharacterModifierStrictnessCategory } from '../characterModifierBaseData';
 import type { ModifierConfigurationBase } from '../configuration';
 import { CharacterModifierBuildConfigurationSchema, type CharacterModifierBuildConfigurationSchemaType } from './configurationBuilder';
 
-export interface CharacterModifierConfigBase {
+/** Base type for a character modifier type definition. */
+export interface CharacterModifierTypeDefinitionBase {
 	readonly typeId: string;
 	readonly visibleName: string;
 	readonly permissionDefault: PermissionConfigDefault;
@@ -13,36 +16,64 @@ export interface CharacterModifierConfigBase {
 	readonly strictnessCategory: CharacterModifierStrictnessCategory;
 }
 
-export interface CharacterModifierConfig<out TType extends string, TConfig extends ModifierConfigurationBase> extends CharacterModifierConfigBase {
+/** Character modifier type configuration data */
+export interface CharacterModifierTypeConstructedDefinition<TType extends string, TConfig extends ModifierConfigurationBase> extends CharacterModifierTypeDefinitionBase {
 	readonly typeId: TType;
 	readonly configurationDefinition: TConfig;
-	readonly configurationSchema: CharacterModifierBuildConfigurationSchemaType<TConfig>;
-}
+	readonly instanceDataSchema: CharacterModifierInstanceDataSchema<TType, TConfig>;
+	readonly effectDataSchema: CharacterModifierEffectDataSchema<TType, TConfig>;
 
+	readonly applyCharacterEffects?: (config: CharacterModifierConfiguration<TConfig>, currentEffects: EffectsDefinition) => Readonly<Partial<EffectsDefinition>>;
+
+	instanceToEffect(instance: z.infer<CharacterModifierInstanceDataSchema<TType, TConfig>>): z.infer<CharacterModifierEffectDataSchema<TType, TConfig>>;
+}
+/** Character modifier type configuration data */
 type CharacterModifierConfiguration<TConfig extends ModifierConfigurationBase> = z.infer<CharacterModifierBuildConfigurationSchemaType<TConfig>>;
 
-/**
- * Category for how strictly is a modifier type perceived.
- * - `normal` - This is a normal modifier, "prompt" permission by default
- * - `strict` - This is a strict modifier, "no" permission by default, but users can still change it to "prompt" or even "yes"
- * - `extreme` - This is a strict modifier, "no" permission by default, and users can only change it to "prompt"
- */
-export type CharacterModifierStrictnessCategory =
-	| 'normal'
-	| 'strict'
-	| 'extreme';
+/** "Common" data for all character modifier instances */
+export const CharacterModifierInstanceCommonDataSchema = z.object({
+	/** Unique identifier */
+	id: z.string(),
+	enabled: z.boolean(),
+});
+type CharacterModifierInstanceDataSchema<TType extends string, TConfig extends ModifierConfigurationBase> = z.ZodObject<z.objectUtil.extendShape<(typeof CharacterModifierInstanceCommonDataSchema extends ZodObject<infer TShape> ? TShape : never), {
+	type: z.ZodLiteral<TType>;
+	config: CharacterModifierBuildConfigurationSchemaType<TConfig>;
+}>>;
+
+/** "Common" data for all character modifier effects */
+export const CharacterModifierEffectCommonDataSchema = z.object({
+	id: z.string(),
+});
+
+type CharacterModifierEffectDataSchema<TType extends string, TConfig extends ModifierConfigurationBase> = z.ZodObject<z.objectUtil.extendShape<(typeof CharacterModifierEffectCommonDataSchema extends ZodObject<infer TShape> ? TShape : never), {
+	type: z.ZodLiteral<TType>;
+	config: CharacterModifierBuildConfigurationSchemaType<TConfig>;
+}>>;
 
 export function DefineCharacterModifier<
 	const TType extends string,
 	const TConfig extends ModifierConfigurationBase,
->(intermediateConfig: {
-	typeId: TType;
-	visibleName: string;
-	strictnessCategory: CharacterModifierStrictnessCategory;
-	config: TConfig;
+>(
+	intermediateConfig: {
+		typeId: TType;
+		visibleName: string;
+		strictnessCategory: CharacterModifierStrictnessCategory;
+		config: TConfig;
+	} & Pick<CharacterModifierTypeConstructedDefinition<TType, TConfig>, 'applyCharacterEffects'>,
+): CharacterModifierTypeConstructedDefinition<TType, TConfig> {
 
-	applyCharacterEffects?: NoInfer<(config: CharacterModifierConfiguration<TConfig>, currentEffects: EffectsDefinition) => Readonly<Partial<EffectsDefinition>>>;
-}): CharacterModifierConfig<TType, TConfig> {
+	const configSchema = CharacterModifierBuildConfigurationSchema(intermediateConfig.config);
+
+	const instanceDataSchema: CharacterModifierInstanceDataSchema<TType, TConfig> = CharacterModifierInstanceCommonDataSchema.extend({
+		type: z.literal(intermediateConfig.typeId),
+		config: configSchema,
+	});
+
+	const effectDataSchema: CharacterModifierEffectDataSchema<TType, TConfig> = CharacterModifierEffectCommonDataSchema.extend({
+		type: z.literal(intermediateConfig.typeId),
+		config: configSchema,
+	});
 
 	return {
 		typeId: intermediateConfig.typeId,
@@ -59,6 +90,19 @@ export function DefineCharacterModifier<
 					intermediateConfig.strictnessCategory === 'extreme' ? ['yes'] :
 						AssertNever(intermediateConfig.strictnessCategory),
 		configurationDefinition: intermediateConfig.config,
-		configurationSchema: CharacterModifierBuildConfigurationSchema(intermediateConfig.config),
+		instanceDataSchema,
+		effectDataSchema,
+		applyCharacterEffects: intermediateConfig.applyCharacterEffects,
+
+		instanceToEffect(instance: z.infer<CharacterModifierInstanceDataSchema<TType, TConfig>>): z.infer<CharacterModifierEffectDataSchema<TType, TConfig>> {
+			Assert(instance.type === intermediateConfig.typeId);
+
+			return {
+				id: instance.id,
+				type: instance.type,
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+				config: cloneDeep(instance.config) as any,
+			};
+		},
 	};
 }

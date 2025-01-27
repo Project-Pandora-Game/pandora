9ce923ac38df5718a60d9939053145058760fedb
Jomshir98 <jomshir98@protonmail.com>
2025-01-27 23:15:34 +0100

    [DEV] Migrate ESLint config to flat format (#890)

--- pandora-client-web/src/common/clipboard.ts
+++ pandora-client-web/src/common/clipboard.ts
@@ -21,7 +21,7 @@ export function CopyToClipboard(text: string, onSuccess?: () => void, onError?: 
 		let successful = false;
 
 		try {
-			// eslint-disable-next-line deprecation/deprecation
+			// eslint-disable-next-line @typescript-eslint/no-deprecated
 			successful = document.execCommand('copy');
 			if (!successful) {
 				logger.warning(`Failed to copy text with returned error by execCommand`);

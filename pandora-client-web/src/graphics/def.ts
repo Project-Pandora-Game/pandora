894cebf4b4f6f6f693131a8be104cb7b20588e27
jomshir98 <jomshir98@protonmail.com>
2025-01-28 16:39:32 +0000

    [REFACTOR] Rework how layer priority ordering calculation is done

--- pandora-client-web/src/graphics/def.ts
+++ pandora-client-web/src/graphics/def.ts
@@ -1,16 +1,15 @@
 import type { Immutable } from 'immer';
-import { useMemo } from 'react';
 import {
 	AppearancePose,
-	ArrayIncludesGuard,
 	Assert,
 	AssertNever,
-	CharacterArmsPose,
+	IsNotNullable,
 	Item,
 	LAYER_PRIORITIES,
 	LayerPriority,
 	type LayerStateOverrides,
 } from 'pandora-common';
+import { useMemo } from 'react';
 import { AssetGraphicsLayer } from '../assets/assetGraphics';
 
 export type LayerState = {
@@ -19,90 +18,86 @@ export type LayerState = {
 	state?: LayerStateOverrides;
 };
 
-export const DOUBLE_ORDERED = [
-	'BELOW_ARM_RIGHT',
-	'BELOW_ARM_LEFT',
-	'ARM_RIGHT',
-	'ARM_LEFT',
-	'ABOVE_ARM_RIGHT',
-	'ABOVE_ARM_LEFT',
-] as const satisfies readonly LayerPriority[];
-type DoubleOrdered = typeof DOUBLE_ORDERED[number];
+export function ComputeLayerPriorityOrder({ view, armsOrder, leftArm, rightArm }: Immutable<AppearancePose>): readonly LayerPriority[] {
+	function ReverseIf(condition: boolean, arr: ((LayerPriority | null)[] | null)[]): (LayerPriority | null)[] {
+		return condition ? arr.reverse().flat() : arr.flat();
+	}
 
-export type ComputedLayerPriority = Exclude<LayerPriority, DoubleOrdered>
-	| `${DoubleOrdered}_BACK`
-	| `${DoubleOrdered}_FRONT`;
+	const order = ([
+		'BACKGROUND',
+		'BELOW_BACK_HAIR',
+		'BACK_HAIR',
 
-type LeftRight = 'LEFT' | 'RIGHT';
+		...(ReverseIf(armsOrder.upper === 'left',
+			[
+				leftArm.position === 'back' ? [
+					`BELOW_ARM_LEFT`,
+					`ARM_LEFT`,
+					`ABOVE_ARM_LEFT`,
+				] : null,
+				rightArm.position === 'back' ? [
+					`BELOW_ARM_RIGHT`,
+					`ARM_RIGHT`,
+					`ABOVE_ARM_RIGHT`,
+				] : null,
+			],
+		)),
 
-export function useComputedLayerPriority({ view, armsOrder }: Immutable<AppearancePose>): readonly ComputedLayerPriority[] {
-	return useMemo(() => {
-		const arms: [LeftRight, LeftRight] = ['LEFT', 'RIGHT'];
+		'BELOW_BODY_SOLES',
+		'BODY_SOLES',
+		'BELOW_BODY',
+		'BODY',
+		'BELOW_BREASTS',
+		'BREASTS',
+		'ABOVE_BODY',
 
-		if (armsOrder.upper === 'left') {
-			arms.reverse();
-		}
+		...(ReverseIf(armsOrder.upper === 'left',
+			[
+				leftArm.position === 'front' ? [
+					`BELOW_ARM_LEFT`,
+					`ARM_LEFT`,
+					`ABOVE_ARM_LEFT`,
+				] : null,
+				rightArm.position === 'front' ? [
+					`BELOW_ARM_RIGHT`,
+					`ARM_RIGHT`,
+					`ABOVE_ARM_RIGHT`,
+				] : null,
+			],
+		)),
 
-		const order = [
-			'BACKGROUND',
-			'BELOW_BACK_HAIR',
-			'BACK_HAIR',
+		'FRONT_HAIR',
+		'ABOVE_FRONT_HAIR',
+		'OVERLAY',
+	] satisfies readonly (LayerPriority | null)[]).filter(IsNotNullable);
 
-			`BELOW_ARM_${arms[0]}_BACK`,
-			`ARM_${arms[0]}_BACK`,
-			`ABOVE_ARM_${arms[0]}_BACK`,
+	Assert(new Set(order).size === order.length);
+	Assert(order.length === LAYER_PRIORITIES.length);
 
-			`BELOW_ARM_${arms[1]}_BACK`,
-			`ARM_${arms[1]}_BACK`,
-			`ABOVE_ARM_${arms[1]}_BACK`,
+	switch (view) {
+		case 'front':
+			return order;
+		case 'back':
+			return order.reverse();
+	}
+	AssertNever(view);
+}
 
-			'BELOW_BODY_SOLES',
-			'BODY_SOLES',
-			'BELOW_BODY',
-			'BODY',
-			'BELOW_BREASTS',
-			'BREASTS',
-			'ABOVE_BODY',
-
-			`BELOW_ARM_${arms[0]}_FRONT`,
-			`ARM_${arms[0]}_FRONT`,
-			`ABOVE_ARM_${arms[0]}_FRONT`,
-
-			`BELOW_ARM_${arms[1]}_FRONT`,
-			`ARM_${arms[1]}_FRONT`,
-			`ABOVE_ARM_${arms[1]}_FRONT`,
-
-			'FRONT_HAIR',
-			'ABOVE_FRONT_HAIR',
-			'OVERLAY',
-		] satisfies readonly ComputedLayerPriority[];
-
-		Assert(new Set(order).size === order.length);
-		Assert(order.length === LAYER_PRIORITIES.length + DOUBLE_ORDERED.length);
-
-		switch (view) {
-			case 'front':
-				return order;
-			case 'back':
-				return order.reverse();
-		}
-		AssertNever(view);
-	}, [view, armsOrder.upper]);
+export function useComputedLayerPriority(pose: Immutable<AppearancePose>): readonly LayerPriority[] {
+	return useMemo((): readonly LayerPriority[] => ComputeLayerPriorityOrder(pose), [pose]);
 }
 
 // Some priority layers need their internal order reversed to make sense
-export const PRIORITY_ORDER_REVERSE_PRIORITIES: ReadonlySet<ComputedLayerPriority> = new Set([
+export const PRIORITY_ORDER_REVERSE_PRIORITIES: ReadonlySet<LayerPriority> = new Set([
 	'BACKGROUND',
 	'BELOW_BACK_HAIR',
 	'BACK_HAIR',
 	'BELOW_BODY_SOLES',
 	'BELOW_BODY',
 	'BELOW_BREASTS',
-	'BELOW_ARM_LEFT_BACK',
-	'BELOW_ARM_RIGHT_BACK',
-	'BELOW_ARM_LEFT_FRONT',
-	'BELOW_ARM_RIGHT_FRONT',
-]);
+	'BELOW_ARM_LEFT',
+	'BELOW_ARM_RIGHT',
+] satisfies LayerPriority[]);
 
 // Some priority layers should get mirrored when layer get mirrored
 export const PRIORITY_ORDER_MIRROR: Partial<Record<LayerPriority, LayerPriority>> = {
@@ -123,23 +118,4 @@ if (!(Object.entries(PRIORITY_ORDER_MIRROR)).every(([original, mirror]) => PRIOR
 export function MirrorPriority(priority: LayerPriority): LayerPriority {
 	const mirrorPriority = PRIORITY_ORDER_MIRROR[priority];
 	return mirrorPriority != null ? mirrorPriority : priority;
-}
-
-export function ComputeLayerPriority(priority: LayerPriority, { leftArm, rightArm }: CharacterArmsPose): ComputedLayerPriority {
-	if (!ArrayIncludesGuard(DOUBLE_ORDERED, priority)) {
-		return priority;
-	}
-
-	switch (priority) {
-		case 'ABOVE_ARM_LEFT':
-		case 'ARM_LEFT':
-		case 'BELOW_ARM_LEFT':
-			return leftArm.position === 'front' ? `${priority}_FRONT` : `${priority}_BACK`;
-		case 'ABOVE_ARM_RIGHT':
-		case 'ARM_RIGHT':
-		case 'BELOW_ARM_RIGHT':
-			return rightArm.position === 'front' ? `${priority}_FRONT` : `${priority}_BACK`;
-	}
-
-	AssertNever(priority);
 }

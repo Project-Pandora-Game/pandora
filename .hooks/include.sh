4be68a63d7860fb79df4af21a4f7215fe112a173
Jomshir98 <jomshir98@protonmail.com>
2025-02-08 18:05:01 +0100

    [FIX] Update npm keys while running pnpm helper

--- .hooks/include.sh
+++ .hooks/include.sh
@@ -4,5 +4,15 @@ function pnpm_helper() {
 	if [ "${CI:-}" == "true" ]; then
 		return
 	fi
+
+	# Update npm keys (ignoring any failures)
+	local npm_keys="$(curl -s https://registry.npmjs.org/-/npm/v1/keys | jq -c '{npm: .keys}' || echo '')"
+	if [[ "$npm_keys" ]]; then
+		export COREPACK_INTEGRITY_KEYS="$npm_keys"
+	fi
+
+	# Do not prompt for user confirmation if downloading new pnpm version
+	export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
 	pnpm install --prefer-offline --frozen-lockfile
 }

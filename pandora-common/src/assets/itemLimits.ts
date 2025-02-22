a902c4a0ee66040ccb7a827662815a41d2167d34
jomshir98 <jomshir98@protonmail.com>
2025-02-22 22:59:21 +0000

    [CHANGE] Increase limits for characters, room inventory and outfit storage

--- pandora-common/src/assets/itemLimits.ts
+++ pandora-common/src/assets/itemLimits.ts
@@ -1,6 +1,6 @@
 /** The maximum number of items a room can store */
-export const ITEM_LIMIT_ROOM_INVENTORY = 100;
+export const ITEM_LIMIT_ROOM_INVENTORY = 150;
 /** The maximum number of items across all saved outfits */
-export const ITEM_LIMIT_ACCOUNT_OUTFIT_STORAGE = 100;
+export const ITEM_LIMIT_ACCOUNT_OUTFIT_STORAGE = 200;
 /** The maximum number of items a character can wear */
-export const ITEM_LIMIT_CHARACTER_WORN = 50;
+export const ITEM_LIMIT_CHARACTER_WORN = 100;

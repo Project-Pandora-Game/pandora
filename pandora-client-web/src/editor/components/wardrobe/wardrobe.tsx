866dbfd9724a1ab8005f411e2c7e91cac5c7532d
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-02-23 13:25:30 +0100

    [CHANGE] default item display name setting is now custom name

--- pandora-client-web/src/editor/components/wardrobe/wardrobe.tsx
+++ pandora-client-web/src/editor/components/wardrobe/wardrobe.tsx
@@ -198,7 +198,7 @@ export function EditorWardrobeContextProvider({ children }: { children: ReactNod
 		actionPreviewState,
 		showExtraActionButtons: true,
 		showHoverPreview: true,
-		itemDisplayNameType: 'custom_with_original_in_brackets',
+		itemDisplayNameType: 'custom',
 	}), [character, assetList, heldItem, scrollToItem, focuser, extraItemActions, actionPreviewState]);
 
 	return (

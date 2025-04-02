110db965cd1a732b9ddc9aadb263b1e4f0cd55d4
jomshir98 <jomshir98@protonmail.com>
2025-04-02 21:37:43 +0000

    [ADD] Show alpha image mask layer sprite in editor Setup view

--- pandora-client-web/src/editor/graphics/layer/setupLayer.tsx
+++ pandora-client-web/src/editor/graphics/layer/setupLayer.tsx
@@ -36,6 +36,8 @@ export function SetupLayerSelected({
 	switch (layer.type) {
 		case 'mesh':
 			return <SetupMeshLayerSelected { ...props } layer={ layer } />;
+		case 'alphaImageMesh':
+			return <SetupAlphaImageMeshLayerSelected { ...props } layer={ layer } />;
 	}
 	return null;
 }
@@ -115,6 +117,84 @@ export function SetupMeshLayerSelected({
 				texture={ texture }
 				tint={ color }
 				alpha={ alpha }
+			/>
+			<Graphics
+				zIndex={ EDITOR_LAYER_Z_INDEX_EXTRA }
+				draw={ drawWireFrame }
+			/>
+		</Container>
+	);
+}
+
+export function SetupAlphaImageMeshLayerSelected({
+	characterState,
+	zIndex,
+	layer,
+}: {
+	characterState: AssetFrameworkCharacterState;
+	zIndex: number;
+	layer: EditorAssetGraphicsLayer<'alphaImageMesh'>;
+}): ReactElement {
+	const item = characterState.items.find((i) => i.asset.id === layer.asset.id) ?? null;
+
+	const definition = useObservable(layer.definition);
+	const {
+		height,
+		width,
+		x, y,
+	} = definition;
+
+	const { points, triangles } = useLayerMeshPoints(definition);
+
+	const evaluator = useAppearanceConditionEvaluator(characterState);
+
+	const {
+		image,
+		imageUv,
+	} = useLayerImageSource(evaluator, definition, item);
+
+	const evaluatorUvPose = useAppearanceConditionEvaluator(characterState, false, imageUv);
+	const uv = useLayerVertices(evaluatorUvPose, points, definition, item, true);
+
+	const drawWireFrame = useCallback((g: PIXI.GraphicsContext) => {
+		// Draw triangles
+		for (let i = 0; i < triangles.length; i += 3) {
+			const poly = [0, 1, 2]
+				.map((p) => triangles[i + p])
+				.flatMap((p) => [uv[2 * p] * width + x, uv[2 * p + 1] * height + y]);
+			g
+				.poly(poly)
+				.stroke({ width: 1, color: 0x333333, alpha: 0.2, pixelLine: true });
+		}
+		// Draw nice points on top of triangles
+		for (const point of points) {
+			g
+				.circle(point.pos[0], point.pos[1], 2.5)
+				.fill({ color: 0xcccccc, alpha: 0.8 })
+				.stroke({ width: 1, color: 0x000000, alpha: 0.8 });
+		}
+	}, [points, triangles, uv, x, y, width, height]);
+
+	const asset = layer.asset;
+	const editorAssetTextures = useObservable(asset.textures);
+
+	const editorGetTexture = useMemo<((image: string) => Texture) | undefined>(() => {
+		return (i) => (editorAssetTextures.get(i) ?? Texture.EMPTY);
+	}, [editorAssetTextures]);
+
+	const texture = useTexture(image, undefined, editorGetTexture);
+
+	return (
+		<Container
+			zIndex={ zIndex }
+			sortableChildren
+		>
+			<Sprite
+				x={ x }
+				y={ y }
+				width={ width }
+				height={ height }
+				texture={ texture }
 			/>
 			<Graphics
 				zIndex={ EDITOR_LAYER_Z_INDEX_EXTRA }

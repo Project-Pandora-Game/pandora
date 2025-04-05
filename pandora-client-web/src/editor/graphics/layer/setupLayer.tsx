ff616ceeedf7c44bb6601fb22ace6e0e20944242
jomshir98 <jomshir98@protonmail.com>
2025-04-05 17:47:55 +0000

    [ADD] Show layer image bounding box in setup view

--- pandora-client-web/src/editor/graphics/layer/setupLayer.tsx
+++ pandora-client-web/src/editor/graphics/layer/setupLayer.tsx
@@ -1,4 +1,5 @@
-import { AssetFrameworkCharacterState } from 'pandora-common';
+import type { Immutable } from 'immer';
+import { AssetFrameworkCharacterState, type LayerImageSetting, type Rectangle } from 'pandora-common';
 import * as PIXI from 'pixi.js';
 import { Texture } from 'pixi.js';
 import { ReactElement, useCallback, useMemo } from 'react';
@@ -8,7 +9,9 @@ import { Container } from '../../../graphics/baseComponents/container.ts';
 import { Graphics } from '../../../graphics/baseComponents/graphics.ts';
 import { Sprite } from '../../../graphics/baseComponents/sprite.ts';
 import { useItemColor, useLayerVertices, type GraphicsLayerProps } from '../../../graphics/layers/graphicsLayerCommon.tsx';
+import { usePixiApp } from '../../../graphics/reconciler/appContext.ts';
 import { useTexture } from '../../../graphics/useTexture.ts';
+import { GetTextureBoundingBox } from '../../../graphics/utility/textureBoundingBox.ts';
 import { useObservable } from '../../../observable.ts';
 import type { EditorAssetGraphicsLayer } from '../../assets/editorAssetGraphicsLayer.ts';
 import { useEditorLayerStateOverride } from '../../editor.tsx';
@@ -51,6 +54,7 @@ export function SetupMeshLayerSelected({
 	zIndex: number;
 	layer: EditorAssetGraphicsLayer<'mesh'>;
 }): ReactElement {
+	const app = usePixiApp();
 	const state = useEditorLayerStateOverride(layer);
 	const item = characterState.items.find((i) => i.asset.id === layer.asset.id) ?? null;
 
@@ -74,7 +78,71 @@ export function SetupMeshLayerSelected({
 	const evaluatorUvPose = useAppearanceConditionEvaluator(characterState, false, imageUv);
 	const uv = useLayerVertices(evaluatorUvPose, points, definition, item, true);
 
+	const asset = layer.asset;
+	const editorAssetTextures = useObservable(asset.textures);
+
+	const editorGetTexture = useMemo<((image: string) => Texture) | undefined>(() => {
+		return (i) => (editorAssetTextures.get(i) ?? Texture.EMPTY);
+	}, [editorAssetTextures]);
+
+	const texture = useTexture(image, undefined, editorGetTexture);
+
+	const { color, alpha } = useItemColor(characterState.items, item, colorizationKey, state);
+
+	const imageBoundingBox = useMemo((): Rectangle | null => {
+		const images = new Set<string>();
+
+		function listImageSettingImages(setting: Immutable<LayerImageSetting>) {
+			setting.overrides.forEach(({ image: i }) => {
+				if (i) {
+					images.add(i);
+				}
+			});
+
+			if (setting.image) {
+				images.add(setting.image);
+			}
+		}
+
+		listImageSettingImages(definition.image);
+		definition.scaling?.stops.forEach((stop) => listImageSettingImages(stop[1]));
+
+		const textures = Array.from(images).map((i) => editorAssetTextures.get(i));
+		if (textures.length === 0)
+			return null;
+
+		const imageBoundingBoxTmp = [1, 1, 0, 0]; // left, top, rightExclusive, bottomExclusive
+		for (const t of textures) {
+			if (t == null)
+				return null;
+
+			const box = GetTextureBoundingBox(t, app);
+			if (box.width === 0 || box.height === 0)
+				continue;
+			imageBoundingBoxTmp[0] = Math.min(imageBoundingBoxTmp[0], box.left / box.width);
+			imageBoundingBoxTmp[1] = Math.min(imageBoundingBoxTmp[1], box.top / box.height);
+			imageBoundingBoxTmp[2] = Math.max(imageBoundingBoxTmp[2], box.rightExclusive / box.width);
+			imageBoundingBoxTmp[3] = Math.max(imageBoundingBoxTmp[3], box.bottomExclusive / box.height);
+		}
+		const x1 = Math.floor(definition.x + imageBoundingBoxTmp[0] * definition.width);
+		const y1 = Math.floor(definition.y + imageBoundingBoxTmp[1] * definition.height);
+		const x2 = Math.ceil(definition.x + imageBoundingBoxTmp[2] * definition.width);
+		const y2 = Math.ceil(definition.y + imageBoundingBoxTmp[3] * definition.height);
+		return {
+			x: x1,
+			y: y1,
+			width: x2 - x1,
+			height: y2 - y1,
+		};
+	}, [app, definition, editorAssetTextures]);
+
 	const drawWireFrame = useCallback((g: PIXI.GraphicsContext) => {
+		// Draw image area
+		if (imageBoundingBox != null) {
+			g
+				.rect(imageBoundingBox.x - 1, imageBoundingBox.y - 1, imageBoundingBox.width + 2, imageBoundingBox.height + 2)
+				.stroke({ width: 1, color: 0x44ff44, alpha: 0.8, pixelLine: true });
+		}
 		// Draw triangles
 		for (let i = 0; i < triangles.length; i += 3) {
 			const poly = [0, 1, 2]
@@ -91,18 +159,7 @@ export function SetupMeshLayerSelected({
 				.fill({ color: 0xcccccc, alpha: 0.8 })
 				.stroke({ width: 1, color: 0x000000, alpha: 0.8, pixelLine: true });
 		}
-	}, [points, triangles, uv, x, y, width, height]);
-
-	const asset = layer.asset;
-	const editorAssetTextures = useObservable(asset.textures);
-
-	const editorGetTexture = useMemo<((image: string) => Texture) | undefined>(() => {
-		return (i) => (editorAssetTextures.get(i) ?? Texture.EMPTY);
-	}, [editorAssetTextures]);
-
-	const texture = useTexture(image, undefined, editorGetTexture);
-
-	const { color, alpha } = useItemColor(characterState.items, item, colorizationKey, state);
+	}, [points, triangles, uv, x, y, width, height, imageBoundingBox]);
 
 	return (
 		<Container
---
+++ pandora-client-web/src/graphics/utility/textureBoundingBox.ts
@@ -0,0 +1,98 @@
+import { freeze } from 'immer';
+import { Assert, type ImageBoundingBox } from 'pandora-common';
+import { Sprite, type Application, type Texture } from 'pixi.js';
+
+const TextureBoundingBoxCache = new WeakMap<Texture, ImageBoundingBox>();
+
+/**
+ * Calculates a bounding box of a texture, including all pixels with non-zero alpha.
+ *
+ * The result is cached.
+ * @param texture - The texture to calculate the box for
+ * @param app - Any Pixi application with multiView enabled
+ * @returns The bounding box
+ */
+export function GetTextureBoundingBox(texture: Texture, app: Application): ImageBoundingBox {
+
+	const cachedResult = TextureBoundingBoxCache.get(texture);
+	if (cachedResult !== undefined)
+		return cachedResult;
+
+	const { width, height } = texture.frame;
+	Assert(width === texture.source.pixelWidth);
+	Assert(height === texture.source.pixelHeight);
+
+	Assert(app.canvas instanceof HTMLCanvasElement, 'Expected app.view to be an HTMLCanvasElement');
+
+	app.renderer.resolution = 1;
+	app.renderer.resize(width, height);
+	app.renderer.background.color = 0x000000;
+	app.renderer.background.alpha = 0;
+
+	app.stage.removeChildren();
+	const sprite = new Sprite(texture);
+	app.stage.addChild(sprite);
+	app.render();
+	app.stage.removeChild(sprite);
+
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	const context = canvas.getContext('2d', { willReadFrequently: true });
+	Assert(context != null);
+	context.clearRect(0, 0, width, height);
+	context.drawImage(app.canvas, 0, 0, width, height);
+	const imageData = context.getImageData(0, 0, width, height, { colorSpace: 'srgb' });
+
+	Assert(imageData.width === width);
+	Assert(imageData.height === height);
+	Assert(imageData.data.length === (4 * width * height));
+
+	let left = width - 1;
+	let top = height - 1;
+	let rightExclusive = 0;
+	let bottomExclusive = 0;
+
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const i = 4 * (y * width + x) + 3;
+
+			// Check if the pixel is non-transparent
+			if (imageData.data[i] > 0) {
+				left = Math.min(left, x);
+				top = Math.min(top, y);
+				rightExclusive = Math.max(rightExclusive, x + 1);
+				bottomExclusive = Math.max(bottomExclusive, y + 1);
+			}
+		}
+	}
+
+	let result: ImageBoundingBox;
+
+	// Special case if the image is empty
+	if (left === (width - 1) && top === (height - 1) && rightExclusive === 0 && bottomExclusive === 0) {
+		result = {
+			left: 0,
+			top: 0,
+			rightExclusive: 0,
+			bottomExclusive: 0,
+			width,
+			height,
+		};
+	} else {
+		Assert(left < rightExclusive);
+		Assert(top < bottomExclusive);
+		result = {
+			left,
+			top,
+			rightExclusive,
+			bottomExclusive,
+			width,
+			height,
+		};
+	}
+
+	freeze(result);
+	TextureBoundingBoxCache.set(texture, result);
+	return result;
+}

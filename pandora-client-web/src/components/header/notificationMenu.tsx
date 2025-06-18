115728c8837f66c3ac1576e4107185fecd023301
jomshir98 <jomshir98@protonmail.com>
2025-06-18 13:33:34 +0000

    [ADD] Notifications menu

---
+++ pandora-client-web/src/components/header/notificationMenu.tsx
@@ -0,0 +1,75 @@
+import classNames from 'classnames';
+import type { Immutable } from 'immer';
+import { CLIENT_NOTIFICATION_TYPES } from 'pandora-common';
+import type { ReactElement } from 'react';
+import crossIcon from '../../assets/icons/cross.svg';
+import deleteIcon from '../../assets/icons/delete.svg';
+import type { NotificationEntry } from '../../services/notificationHandler.tsx';
+import { useService } from '../../services/serviceProvider.tsx';
+import { Button, IconButton } from '../common/button/button.tsx';
+import { Column, Row } from '../common/container/container.tsx';
+import { DialogInPortal } from '../dialog/dialog.tsx';
+import './notificationMenu.scss';
+
+export function NotificationMenu({ visible, close, notifications, clearNotifications }: {
+	visible: boolean;
+	close: () => void;
+	notifications: Immutable<NotificationEntry[]>;
+	clearNotifications: () => void;
+}): ReactElement {
+	const notificationHandler = useService('notificationHandler');
+
+	return (
+		<DialogInPortal priority={ 6 } location='mainOverlay'>
+			<Column className={ classNames('OverlayNotificationMenu', visible ? null : 'hide') } padding='medium'>
+				<Row alignX='space-between'>
+					<IconButton
+						onClick={ clearNotifications }
+						theme='default'
+						className='topButton'
+						src={ deleteIcon }
+						alt='Clear notifications'
+						disabled={ notifications.length === 0 }
+					/>
+					<Button
+						onClick={ close }
+						theme='default'
+						slim
+					>
+						Close ►
+					</Button>
+				</Row>
+				<Column className='content' overflowX='auto' overflowY='auto' padding='medium'>
+					{
+						notifications.length > 0 ? (
+							Array.from(notifications).reverse().map((n, i) => (
+								<Column key={ i } className='notificationEntry' alignY='start' padding='medium'>
+									<Row>
+										<strong className='zero-width flex-1 title'>{ n.title ?? CLIENT_NOTIFICATION_TYPES[n.type].name }</strong>
+										<IconButton
+											onClick={ () => {
+												notificationHandler.dismissNotification(n);
+											} }
+											theme='transparent'
+											className='dismissButton'
+											src={ crossIcon }
+											alt='Dismiss notification'
+											slim
+										/>
+									</Row>
+									{
+										n.content ? (
+											<p>{ n.content }</p>
+										) : null
+									}
+								</Column>
+							))
+						) : (
+							<span className='noNotificationsInfo'>You have no notifications at the moment</span>
+						)
+					}
+				</Column>
+			</Column>
+		</DialogInPortal>
+	);
+}

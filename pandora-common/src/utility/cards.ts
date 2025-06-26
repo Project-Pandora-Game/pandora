a2e5c934be119cebf49bcc2c7224b9c6352c7814
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2025-06-26 19:35:35 +0200

    [CHANGE] More comments, T is now 10

--- pandora-common/src/utility/cards.ts
+++ pandora-common/src/utility/cards.ts
@@ -10,7 +10,7 @@ const suits = [
 	'\u2667', //clubs
 	'\u2664', //spades
 ] as const;
-const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'] as const;
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] as const;
 
 // Define allowed values
 const SuitSchema = z.enum(suits);
@@ -27,20 +27,33 @@ export class CardGameCard {
 	public readonly rank: CardGameRank;
 	private faceDown: boolean;
 
+	/**
+	 * @returns {string}
+	 */
 	public toString() {
 		return `${this.rank}${this.suit}`;
 	}
 
+	/**
+	 * Change the state of a card, so that it is listed as "revealed"
+	 */
 	public reveal() {
-		// Change the state of a card, so that it is listed as "revealed"
 		this.faceDown = false;
 	}
 
-	public isReveiled() {
-		// Return the state of a card
+	/**
+	 * Return the state of a card
+	 * @returns {boolean}
+	 */
+	public isRevealed() {
 		return !this.faceDown;
 	}
 
+	/**
+	 * Creates an instance of CardGameCard.
+	 * @param {CardGameSuit} s
+	 * @param {CardGameRank} r
+	 */
 	constructor(s: CardGameSuit, r: CardGameRank) {
 		this.suit = s;
 		this.rank = r;
@@ -48,9 +61,14 @@ export class CardGameCard {
 	}
 }
 
-// Helper functions
+/**
+ * Helper function.
+ * Creates a string of given card arrays
+ * @export
+ * @param {...CardArray[]} cardGroups
+ * @returns {string}
+ */
 export function CardArraysToString(...cardGroups: CardArray[]) {
-	//Creates a string out of given card arrays
 	const cards = cardGroups.flat();
 	return cards.length === 0 ? 'nothing' : cards.map((c) => c.toString()).join(', ');
 }
@@ -67,13 +85,18 @@ class CardDeck {
 		ShuffleArray(this.deck);
 	}
 
+	/**
+	 * Returns how many cards are left in the deck
+	 * @readonly
+	 */
 	public get cardsLeft() {
-		// How many cards are left in the deck
 		return this.deck.length;
 	}
 
+	/**
+	 * Cretae a new shuffled deck of cards
+	 */
 	public create() {
-		// Cretae a new shuffled deck of cards
 		for (const suit of suits) {
 			for (const rank of ranks) {
 				this.addCard(new CardGameCard(suit, rank));
@@ -82,11 +105,19 @@ class CardDeck {
 		this.shuffle();
 	}
 
+	/**
+	 * Returns the next card from the top of the deck
+	 * @returns CardGameCard | undefined
+	 */
 	public deal() {
-		// Returns the next card from the top of the deck
+		//
 		return this.deck.length > 0 ? this.deck.pop() : undefined;
 	}
 
+	/**
+	 * Creates an instance of CardDeck.
+	 * @param {CardGameCard[]} [cards]
+	 */
 	constructor(cards?: CardGameCard[]) {
 		// Parameter used solely for debugging puposes
 		if (cards) {
@@ -101,29 +132,47 @@ class CardPlayer {
 	private id: CharacterId;
 	private hand: CardArray = [];
 
+	/**
+	 * Returns the player's id
+	 * @returns `c${number}`
+	 */
 	public getId() {
-		// Returns the player's id
 		return this.id;
 	}
 
+	/**
+	 * Store a card that is dealt either face up or face down
+	 * @param {CardGameCard} c
+	 * @param {boolean} [open=false]
+	 */
 	public receiveCard(c: CardGameCard, open: boolean = false) {
-		// Store a card that is dealt either face up or face down
 		if (open) c.reveal();
 		this.hand.push(c);
 	}
 
+	/**
+	 * Returns an array of cards. Either all reveiled or all hidden cards
+	 * @param {boolean} revealed
+	 * @returns CardArray
+	 */
 	public getCards(revealed: boolean) {
-		// Returns an array of cards. Either all reveiled or all hidden cards
-		const cards = revealed ? this.hand.filter((c) => c.isReveiled()) : this.hand.filter((c) => !c.isReveiled());
+		const cards = revealed ? this.hand.filter((c) => c.isRevealed()) : this.hand.filter((c) => !c.isRevealed());
 		return cards;
 	}
 
+	/**
+	 * Returns an array of all cards the player currently holds and sets their status to 'reveiled'
+	 * @returns CardArray
+	 */
 	public showHand() {
-		// Returns an array of all cards the player currently holds and sets their status to 'reveiled'
 		this.hand.forEach((c) => c.reveal());
 		return CardArraysToString(this.hand);
 	}
 
+	/**
+	 * Creates an instance of CardPlayer.
+	 * @param {CharacterId} id
+	 */
 	constructor(id: CharacterId) {
 		this.id = id;
 	}
@@ -137,8 +186,12 @@ export class CardGameGame {
 	private players: CardPlayer[];
 	private public: boolean;
 
+	/**
+	 * Add a player to the game, if they didn't join already
+	 * @param {CharacterId} c
+	 * @returns true or false, depending on if the player has been added or not
+	 */
 	public joinGame(c: CharacterId) {
-		// Add a player to the game, if they didn't join already
 		if (!this.players.some((p) => p.getId() === c)) {
 			this.players.push(new CardPlayer(c));
 			return true;
@@ -147,15 +200,24 @@ export class CardGameGame {
 		}
 	}
 
+	/**
+	 * Remove a player from the list of players
+	 * @param {CharacterId} c
+	 */
 	public leaveGame(c: CharacterId) {
-		// Remove a player from the list of players
 		this.players = this.players.filter((p) => p.getId() !== c);
 	}
 
+	/**
+	 * Deal a number of cards, either to a player or to the current space (c == null)
+	 * A card can be dealt either open or face down
+#
+	 * @param {number} n
+	 * @param {CharacterId} [c]
+	 * @param {boolean} [open=false]
+	 * @returns {(CardArray | null)}
+	 */
 	public dealTo(n: number, c?: CharacterId, open: boolean = false): CardArray | null {
-		// Deal a number of cards, either to a player or to the current space (c == null)
-		// A card can be dealt either open or face down
-
 		if (n > this.deck.cardsLeft) return null;
 		//Deal a card either to the room or to a player
 		const cards: CardArray = [];
@@ -176,19 +238,31 @@ export class CardGameGame {
 		return cards;
 	}
 
+	/**
+	 * Make a player show their current cards
+	 * @param {CharacterId} c
+	 */
 	public revealHand(c: CharacterId) {
-		// Make a player show their current cards
 		this.players.find((p) => p.getId() === c)?.showHand();
 	}
 
+	/**
+	 * Check, if the given player is part of the game
+	 * @param {CharacterId} c
+	 * @returns {boolean}
+	 */
 	public isPlayer(c: CharacterId) {
-		// Check, if the given player is part of the game
 		return this.players.find((p) => p.getId() === c);
 	}
 
+	/**
+	 * Get the cards, a player is holding. Either all cards or only those
+	 * that have been revealed previously
+	 * @param {CharacterId} c
+	 * @param {boolean} revealedOnly
+	 * @returns {string} A list of cards
+	 */
 	public getPlayerHand(c: CharacterId, revealedOnly: boolean) {
-		// Get the cards, a player is holding. Either all cards or only those
-		// that have been revealed previously
 		const player = this.players.find((p) => p.getId() === c);
 		if (!player)
 			return 'Nothing';
@@ -197,34 +271,56 @@ export class CardGameGame {
 			: CardArraysToString(player.getCards(true), player.getCards(false));
 	}
 
+	/**
+	 * Returns a list of all current players
+	 * @returns {`c${number}`[]}
+	 */
 	public getPlayerIds() {
-		// Returns a list of all current players
 		return this.players.map((p) => p.getId());
 	}
 
+	/**
+	 * Returns the cards that are on the table
+	 * @returns {string}
+	 */
 	public getSpaceHand() {
-		// Returns the cards that are on the table
 		return CardArraysToString(this.spaceHand);
 	}
 
+	/**
+	 * Checks, if the current player is the game's dealer
+	 * @param {CharacterId} p The player to check
+	 * @returns {boolean}
+	 */
 	public isDealer(p: CharacterId) {
-		// Checks, if the current player is the game's dealer
 		return p === this.dealer;
 	}
 
+	/**
+	 * Get the game's dealer ID
+	 * @returns {`c${number}`}
+	 */
 	public getDealerId() {
-		// Get the game's dealer ID
 		return this.dealer;
 	}
 
+	/**
+	 * Returns true, if the game is public
+	 * @returns {boolean}
+	 */
 	public isPublic() {
-		// True, if it's a public game
 		return this.public;
 	}
 
+	/**
+	 * Creates a game, sets the game to be either public or private and
+	 * can also take a preset deck of cards (currently used for testing only atm)
+	 * @param {CharacterId} dealer
+	 * @param {boolean} p
+	 * @param {CardDeck} [d]
+	 * @memberof CardGameGame
+	 */
 	constructor(dealer: CharacterId, p: boolean, d?: CardDeck) {
-		// Creates a game, sets the game to be either public or private and
-		// can also take a preset deck of cards (currently used for testing only atm)
 		this.players = [];
 		this.spaceHand = [];
 		this.public = p;
@@ -241,7 +337,6 @@ export class CardGameGame {
 }
 
 // The commands for the CardGame
-
 export const CardGameActionSchema = z.discriminatedUnion('action', [
 	z.object({
 		// Create the game, private or public

520d598e982e0844f643af8a93c195d87bf8db63
Jomshir98 <jomshir98@protonmail.com>
2025-08-05 16:57:45 +0200

    [FIX] Update types for new TS version

--- pandora-client-web/src/crypto/helpers.ts
+++ pandora-client-web/src/crypto/helpers.ts
@@ -3,14 +3,14 @@ import { PASSWORD_PREHASH_SALT } from 'pandora-common';
 const crypto = globalThis.crypto;
 const subtle = globalThis.crypto.subtle;
 
-export const Encode = (str: string): Uint8Array => new TextEncoder().encode(str);
+export const Encode = (str: string): Uint8Array<ArrayBuffer> => new TextEncoder().encode(str);
 export const Decode = (buf: Uint8Array): string => new TextDecoder().decode(buf);
 
 export function ArrayToBase64(array: Iterable<number> | ArrayLike<number>): string {
 	return btoa(String.fromCharCode(...Array.from(array)));
 }
 
-export function Base64ToArray(str: string): Uint8Array {
+export function Base64ToArray(str: string): Uint8Array<ArrayBuffer> {
 	return new Uint8Array(Array.from(atob(str).split(''), (c) => c.charCodeAt(0)));
 }
 
@@ -24,7 +24,7 @@ export async function HashSHA256Base64(text: string) {
 	return ArrayToBase64(new Uint8Array(hashBuffer));
 }
 
-export function GenerateIV(base64?: string): { iv: string; alg: { name: 'AES-GCM'; iv: Uint8Array; }; } {
+export function GenerateIV(base64?: string): { iv: string; alg: { name: 'AES-GCM'; iv: Uint8Array<ArrayBuffer>; }; } {
 	if (base64 === undefined) {
 		const iv = crypto.getRandomValues(new Uint8Array(16));
 		return { iv: ArrayToBase64(iv), alg: { name: 'AES-GCM', iv } };

86bc44dd9d488cb17c94278301838c4e43f65658
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-08-13 08:41:04 +0200

    [CHANGE] description of block entering rooms modifier

--- pandora-common/src/gameLogic/characterModifiers/modifierTypes/block_entering_specific_rooms.ts
+++ pandora-common/src/gameLogic/characterModifiers/modifierTypes/block_entering_specific_rooms.ts
@@ -8,7 +8,7 @@ export const block_entering_specific_rooms = DefineCharacterModifier({
 	description: `
 This modifier makes the character unable to move to rooms with the set names. This is space independent, but you can use activation conditions to apply this only to specific spaces. Matching is done against both the room's name (case insensitive) and its unique identifier.
 
-Note that this modifier will not work well on owners and admins within a space as they can move to all rooms in a space directly (not only to neighboring rooms) and can rename/delete/create rooms at any time.
+Note that this modifier will not work well on owners and admins within a space on its own as they can move to all rooms in a space directly (not only to neighboring rooms) and can rename/delete/create rooms at any time. There is another character modifier to prevent managing room maps, which can deal with this "issue".
 	`,
 	strictnessCategory: 'normal',
 	config: {

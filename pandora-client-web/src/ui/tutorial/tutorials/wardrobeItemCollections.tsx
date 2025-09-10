f8447a4da84063ec9208b57fc7fa1efc8bd58b55
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-09-10 18:49:43 +0200

    [ADD] two new tutorials on advanced item interactions

---
+++ pandora-client-web/src/ui/tutorial/tutorials/wardrobeItemCollections.tsx
@@ -0,0 +1,26 @@
+import type { TutorialConfig } from '../tutorialSystem/tutorialConfig.ts';
+
+export const TUTORIAL_WARDROBE_ITEM_COLLECTIONS: TutorialConfig = {
+	id: 'wardrobeItemCollections',
+	name: `Character interactions: Item collections`,
+	disabled: 'workInProgress',
+	description: (
+		<p>
+			This tutorial will teach you how you can save any kind of items together in an item collection to save inside Pandora or for exporting as a template.
+		</p>
+	),
+	stages: [
+		{
+			steps: [
+				{
+					text: (
+						<>
+							[ WORK IN PROGRESS ]
+						</>
+					),
+					conditions: [{ type: 'next' }],
+				},
+			],
+		},
+	],
+};

c8e26b8fdea97d3b256a3e5ec7eb5bc01fdf7b3e
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-09-11 07:07:09 +0200

    [CHANGE] Large tutorial renaming

--- pandora-client-web/src/ui/tutorial/tutorials/wardrobeItemCollections.tsx
+++ pandora-client-web/src/ui/tutorial/tutorials/wardrobeItemCollections.tsx
@@ -2,11 +2,11 @@ import type { TutorialConfig } from '../tutorialSystem/tutorialConfig.ts';
 
 export const TUTORIAL_WARDROBE_ITEM_COLLECTIONS: TutorialConfig = {
 	id: 'wardrobeItemCollections',
-	name: `Character interactions: Item collections`,
+	name: `Item collections`,
 	disabled: 'workInProgress',
 	description: (
 		<p>
-			This tutorial will teach you how you can save any kind of items together in an item collection to save inside Pandora or for exporting as a template.
+			This tutorial explains how you can save any kind of items together in an item collection to save inside Pandora or for exporting as a template.
 		</p>
 	),
 	stages: [

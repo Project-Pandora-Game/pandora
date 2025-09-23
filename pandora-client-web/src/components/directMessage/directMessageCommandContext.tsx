92e8f5a41d0fdca5569992676df2abdd993edf67
Jomshir98 <jomshir98@protonmail.com>
2025-09-23 15:36:02 +0000

    [CHANGE] Change DM invite command to use async handling

--- pandora-client-web/src/components/directMessage/directMessageCommandContext.tsx
+++ pandora-client-web/src/components/directMessage/directMessageCommandContext.tsx
@@ -26,30 +26,26 @@ export const DIRECT_MESSAGE_COMMANDS: readonly IClientCommand<DirectMessageComma
 		description: 'Creates an invite link to your current space',
 		longDescription: 'Creates an invite link to your current space, this invite is limited to the account and has a single use.',
 		handler: CreateDMCommand()
-			.handler(({ directoryConnector, gameState, chat, displayError, sendMessage }) => {
-				(async () => {
-					if (gameState?.currentSpace.value.id == null) {
-						displayError?.('You are not in a space');
-						return;
-					}
+			.handler(async ({ directoryConnector, gameState, chat, displayError, sendMessage }) => {
+				if (gameState?.currentSpace.value.id == null) {
+					displayError?.('You are not in a space');
+					return false;
+				}
 
-					const spaceId = gameState.currentSpace.value.id;
-					const resp = await directoryConnector.awaitResponse('spaceInvite', {
-						action: 'create',
-						data: {
-							type: 'joinMe',
-							accountId: chat.id,
-						},
-					});
-					if (resp.result === 'created') {
-						await sendMessage(`https://project-pandora.com/space/join/${encodeURIComponent(spaceId)}?invite=${encodeURIComponent(resp.invite.id)}`);
-					} else {
-						displayError?.('Failed to create invite: ' + resp.result);
-					}
-				})().catch((e) => {
-					if (e instanceof Error)
-						displayError?.(`Failed to create invite: ${e.message}`);
+				const spaceId = gameState.currentSpace.value.id;
+				const resp = await directoryConnector.awaitResponse('spaceInvite', {
+					action: 'create',
+					data: {
+						type: 'joinMe',
+						accountId: chat.id,
+					},
 				});
+				if (resp.result === 'created') {
+					await sendMessage(`https://project-pandora.com/space/join/${encodeURIComponent(spaceId)}?invite=${encodeURIComponent(resp.invite.id)}`);
+					return true;
+				}
+				displayError?.('Failed to create invite: ' + resp.result);
+				return false;
 			}),
 	},
 ];

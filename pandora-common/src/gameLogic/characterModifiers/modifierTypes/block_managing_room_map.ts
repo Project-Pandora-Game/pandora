b77b1ef4356bf1a8cd29fa9565dc1b04853c63df
Jomshir98 <jomshir98@protonmail.com>
2025-10-16 20:12:06 +0000

    [ADD] "Prevent managing room maps of spaces" should block changes to room settings

--- pandora-common/src/gameLogic/characterModifiers/modifierTypes/block_managing_room_map.ts
+++ pandora-common/src/gameLogic/characterModifiers/modifierTypes/block_managing_room_map.ts
@@ -5,14 +5,19 @@ export const block_managing_room_map = DefineCharacterModifier({
 	typeId: 'block_managing_room_map',
 	visibleName: `Block: Prevent managing room maps of spaces`,
 	description: `
-This modifier prevents the character from being able to add, remove, reposition, and rename any rooms on a space's room grid, even when they are an owner or admin of the space. Changes to a room's background, walls, or geometry are blocked as well.
+This modifier prevents the character from being able to add, remove, reposition, and rename any rooms on a space's room grid, even when they are an owner or admin of the space. Changes to a room's background, walls, settings, or geometry are blocked as well.
+Also prevents modifying default room settings.
 	`,
 	strictnessCategory: 'strict',
 	config: {},
 
 	checkCharacterAction(_config, action, _player, _result) {
-		if (action.type === 'spaceRoomLayout' || action.type === 'roomConfigure')
+		if (action.type === 'spaceRoomLayout' ||
+			action.type === 'roomConfigure' ||
+			(action.type === 'spaceConfigure' && action.globalRoomSettings != null)
+		) {
 			return 'block';
+		}
 
 		return 'allow';
 	},

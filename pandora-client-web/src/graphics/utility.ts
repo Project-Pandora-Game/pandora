36c4404bf02f2a099ad19c72808f63e73a8388b6
Jomshir98 <jomshir98@protonmail.com>
2025-10-21 19:12:15 +0000

    [REFACTOR] Move most of pose transform evaluation to pandora-common

--- pandora-client-web/src/graphics/utility.ts
+++ pandora-client-web/src/graphics/utility.ts
@@ -2,26 +2,8 @@ import type { Immutable } from 'immer';
 import { Assert, type AssetId, type AtomicCondition, type Condition } from 'pandora-common';
 import { ImageSource, type TextureSource } from 'pixi.js';
 
-export function GetAngle(x: number, y: number): number {
-	const angle = Math.atan2(y, x);
-	const degrees = 180 * angle / Math.PI;
-	return (360 + Math.round(degrees)) % 360;
-}
-
-export function RotateVector(x: number, y: number, angle: number): [number, number] {
-	angle *= (Math.PI / 180);
-	const cos = Math.cos(angle);
-	const sin = Math.sin(angle);
-	return [x * cos - y * sin, x * sin + y * cos];
-}
-
 export function EvaluateCondition(condition: Immutable<Condition>, evaluate: (condition: Immutable<AtomicCondition>) => boolean): boolean {
 	return condition.some((clause) => clause.every(evaluate));
-}
-
-export function Conjunction<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): boolean {
-	if (a.size > b.size) [a, b] = [b, a];
-	return [...a].some((x) => b.has(x));
 }
 
 export function StripAssetIdPrefix(id: AssetId): string {
---
+++ pandora-common/src/assets/graphics/transform/index.ts
@@ -0,0 +1,1 @@
+export * from './characterPoseTransforms.ts';
